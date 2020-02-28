1e0075b0ea280f85ac109e6626e20dc83e71ac21
Paulvanderlaan <P.J.vanderlaan-1@student.tudelft.nl>
2020-02-28 13:37:38 +0100

    Cleanup

--- demo-android/src/main/java/nl/tudelft/ipv8/android/demo/ui/bitcoin/BitcoinFragment.kt
+++ demo-android/src/main/java/nl/tudelft/ipv8/android/demo/ui/bitcoin/BitcoinFragment.kt
@@ -9,32 +9,15 @@ import kotlinx.android.synthetic.main.fragment_bitcoin.*
 import nl.tudelft.ipv8.android.demo.R
 import nl.tudelft.ipv8.android.demo.ui.BaseFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [BitcoinFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class BitcoinFragment : BaseFragment(R.layout.fragment_bitcoin) {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     private var publicKeyReceiver: String = ""
     private var bitcoinPrivateKey: String = ""
     private var transactionAmount: Double = 0.0
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -60,18 +43,9 @@ class BitcoinFragment : BaseFragment(R.layout.fragment_bitcoin) {
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment bitcoinFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BitcoinFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = BitcoinFragment()
     }
 }
