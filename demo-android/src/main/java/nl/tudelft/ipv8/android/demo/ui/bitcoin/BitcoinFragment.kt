5b65c30a0486926c8d936dda3a5c22c53384ab6e
Paulvanderlaan <P.J.vanderlaan-1@student.tudelft.nl>
2020-02-28 13:28:54 +0100

    Refactor bitcoin fragment

--- demo-android/src/main/java/nl/tudelft/ipv8/android/demo/ui/bitcoin/BitcoinFragment.kt
+++ demo-android/src/main/java/nl/tudelft/ipv8/android/demo/ui/bitcoin/BitcoinFragment.kt
@@ -5,9 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_bitcoin.*
 import nl.tudelft.ipv8.android.demo.R
+import nl.tudelft.ipv8.android.demo.ui.BaseFragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +16,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [bitcoinFragment.newInstance] factory method to
+ * Use the [BitcoinFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class bitcoinFragment : Fragment() {
+class BitcoinFragment : BaseFragment(R.layout.fragment_bitcoin) {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -67,7 +67,7 @@ class bitcoinFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            bitcoinFragment().apply {
+            BitcoinFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
