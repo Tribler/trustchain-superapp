7c13cf3b29e45f2fc47b48b56b711478a62376bc
Erik Dymbe <erik.dymbe@gmail.com>
2020-03-12 17:57:51 +0100

    Make currency somewhat consistent
    Self-signed transactions print money. Probably not secure yet.

--- trustchain-trader/src/main/java/nl/tudelft/trustchain/trader/ui/TraderFragment.kt
+++ trustchain-trader/src/main/java/nl/tudelft/trustchain/trader/ui/TraderFragment.kt
@@ -4,30 +4,60 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.findNavController
-import kotlinx.android.synthetic.main.fragment_transfer_receive.*
+import kotlinx.android.synthetic.main.fragment_trader.*
 import nl.tudelft.ipv8.attestation.trustchain.BlockListener
 import nl.tudelft.ipv8.attestation.trustchain.TrustChainBlock
 import nl.tudelft.ipv8.attestation.trustchain.store.TrustChainStore
 import nl.tudelft.ipv8.attestation.trustchain.validation.TransactionValidator
+import nl.tudelft.ipv8.util.toHex
 import nl.tudelft.trustchain.common.ui.BaseFragment
 import nl.tudelft.trustchain.trader.R
+import nl.tudelft.trustchain.trader.constants.BlockTypes
+import nl.tudelft.trustchain.trader.util.getAmount
+import nl.tudelft.trustchain.trader.util.getBalance
+import nl.tudelft.trustchain.trader.util.getMyPublicKey
 
+@ExperimentalUnsignedTypes
 class TraderFragment : BaseFragment(R.layout.fragment_trader) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         lifecycleScope.launchWhenStarted {
             val trustchain = getTrustChainCommunity()
 
-            trustchain.registerTransactionValidator("demo_tx_block", object : TransactionValidator {
-                override fun validate(
-                    block: TrustChainBlock,
-                    database: TrustChainStore
-                ): Boolean {
-                    return block.transaction["amount"] != null
-                }
-            })
+            val currentBalance = trustchain.database.getBalance(trustchain.getMyPublicKey())
+            txtBalance.text = "Your balance: ${currentBalance}"
 
-            trustchain.addListener("demo_tx_block", object : BlockListener {
+            trustchain.registerTransactionValidator(
+                BlockTypes.DEMO_TX_BLOCK.value,
+                object : TransactionValidator {
+                    override fun validate(
+                        block: TrustChainBlock,
+                        database: TrustChainStore
+                    ): Boolean {
+                        // Self signed blocks print money, they are always valid
+                        if (block.isSelfSigned) {
+                            return true
+                        }
+
+                        val amount = block.getAmount()
+
+                        if (block.isProposal) {
+                            val balance =
+                                database.getBalance(block.publicKey, block.sequenceNumber - 1u)
+                            return balance > amount
+                        } else {
+                            // Is agreement block
+                            val proposalBlock = database.getLinked(block) ?: return false
+                            if (block.transaction == proposalBlock.transaction) {
+                                return true
+                            }
+                            val balance =
+                                database.getBalance(block.linkPublicKey, block.sequenceNumber - 1u)
+                            return balance > amount
+                        }
+                    }
+                })
+
+            trustchain.addListener(BlockTypes.DEMO_TX_BLOCK.value, object : BlockListener {
                 override fun onBlockReceived(block: TrustChainBlock) {
                     Log.d(
                         "TrustChainDemo",
