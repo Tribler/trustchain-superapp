6a422075724c8b354eae55edb08a6fb63570aff3
tijmen104 <tijmen104@gmail.com>
2020-03-13 12:17:15 +0100

    Temporary market community

---
+++ trustchain-trader/src/main/java/nl/tudelft/trustchain/trader/ui/MarketCommunity.kt
@@ -0,0 +1,8 @@
+package nl.tudelft.trustchain.trader.ui
+
+import nl.tudelft.ipv8.Community
+
+class MarketCommunity : Community() {
+    override val serviceId = "4c69624e61434c504b3ab5bb7dc5a3a61de442585122b24c9f752469a212dc6d8ffa3d42bbf9c2f8d10ba569b270f615ef78aeff0547f38745d22af268037ad64935ee7c054b7921b23b"
+
+}
