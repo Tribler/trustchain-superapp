59cd919ba57e1ee43dd9ed7ff7cd9c52716eb7bc
Paulvanderlaan <P.J.vanderlaan-1@student.tudelft.nl>
2020-03-13 15:16:20 +0100

    View updates and add unique ids

--- demo-android/src/main/java/nl/tudelft/ipv8/android/demo/ui/bitcoin/MySharedWalletsFragment.kt
+++ demo-android/src/main/java/nl/tudelft/ipv8/android/demo/ui/bitcoin/MySharedWalletsFragment.kt
@@ -1,10 +1,13 @@
 package nl.tudelft.ipv8.android.demo.ui.bitcoin
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_join_network.*
+import nl.tudelft.ipv8.android.demo.CoinCommunity
 import nl.tudelft.ipv8.android.demo.R
 import nl.tudelft.ipv8.android.demo.ui.BaseFragment
 
@@ -18,12 +21,19 @@ class MySharedWalletFragment(
     override val controller: BitcoinViewController
 ) : BitcoinView, BaseFragment(R.layout.fragment_my_shared_wallets) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private fun initListView() {
+        val sharedWalletBlocks = getCoinCommunity().fetchLatestJoinedSharedWalletBlocks()
+
+        val adaptor = SharedWalletListAdapter(this.requireView(), sharedWalletBlocks)
+        list_view.adapter = adaptor
+        list_view.setOnItemClickListener { _, view, position, id ->
+            Log.i("Coin", "Clicked: $view, $position, $id")
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        initListView()
     }
 
     override fun onCreateView(
