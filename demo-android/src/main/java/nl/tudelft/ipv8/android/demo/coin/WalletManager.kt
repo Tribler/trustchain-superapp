53a2d0635e8acde2f8c508ef89647259b304c2f4
Brian <b.o.s.planje@student.tudelft.nl>
2020-03-17 20:17:08 +0100

    Add method to create multisignature wallet with Trustchain serialized messages

--- demo-android/src/main/java/nl/tudelft/ipv8/android/demo/coin/WalletManager.kt
+++ demo-android/src/main/java/nl/tudelft/ipv8/android/demo/coin/WalletManager.kt
@@ -3,6 +3,8 @@ package nl.tudelft.ipv8.android.demo.coin
 import android.util.Log
 import com.google.common.base.Joiner
 import info.blockchain.api.blockexplorer.BlockExplorer
+import nl.tudelft.ipv8.util.hexToBytes
+import nl.tudelft.ipv8.util.toHex
 import org.bitcoinj.core.*
 import org.bitcoinj.core.ECKey.ECDSASignature
 import org.bitcoinj.core.listeners.DownloadProgressTracker
@@ -14,6 +16,7 @@ import org.bitcoinj.script.Script
 import org.bitcoinj.script.ScriptBuilder
 import org.bitcoinj.script.ScriptPattern
 import org.bitcoinj.wallet.DeterministicSeed
+import org.bitcoinj.wallet.SendRequest
 import java.io.File
 import java.util.*
 
@@ -95,9 +98,70 @@ class WalletManager(
         Log.i("Coin", "Coin: ${kit.wallet()}")
     }
 
+    // The protocol key we are using (private + public).
+    private fun protocolECKey(): ECKey {
+        return kit.wallet().issuedReceiveKeys[0]
+    }
+
+    // Network: public key representing us we can send over network (public).
+    fun networkPublicECKeyHex(): String {
+        return protocolECKey().publicKeyAsHex
+    }
+
+    fun startNewWalletProcess(
+        networkPublicHexKeys: List<String>,
+        entranceFee: Coin,
+        threshHold: Int = networkPublicHexKeys.size + 1
+    ): String? {
+
+        Log.i("Coin", "Coin: we are making a new multi-sig wallet, with some other people.")
+        val us = protocolECKey()
+        val them = networkPublicHexKeys.map { publicHexKey: String ->
+            ECKey.fromPublicOnly(publicHexKey.hexToBytes())
+        }
+        val total = them.toMutableList()
+        total.add(us)
+
+        Log.i("Coin", "Coin: we will now make a wallet for ${total.size} people in total.")
+        val contract = createMultiSignatureWallet(total, entranceFee, threshHold, params)
+
+        Log.i("Coin", "Coin: your inputs will now be matched to entrance and fees.")
+        val req = SendRequest.forTx(contract)
+        kit.wallet().completeTx(req)
+
+        val transactionId = req.tx.txId.toString()
+        Log.i("Coin", "Coin: the transaction ID will be: ${transactionId}")
+
+        Log.i("Coin", "Coin: we will broadcast your new multi-sig transaction.")
+        val broadcastTransaction = kit.peerGroup().broadcastTransaction(req.tx)
+        broadcastTransaction.setProgressCallback({ progress ->
+            Log.i("Coin", "Coin: broadcast progress is ${progress}.")
+        })
+        broadcastTransaction.broadcast().get()
+        Log.i("Coin", "Coin: successfully broad-casted the multi-sig wallet.")
+
+        val serializedTransaction = attemptToGetTransactionAndSerialize(transactionId)
+
+        return serializedTransaction
+    }
+
+    fun attemptToGetTransactionAndSerialize(transactionId: String): String? {
+        val transaction = kit.wallet().getTransaction(Sha256Hash.wrap(transactionId))
+        if (transaction != null) {
+            val serializedTransaction = transaction.bitcoinSerialize().toHex()
+            Log.i("Coin", "Coin: the serialized transaction is:")
+            Log.i("Coin", "Coin: $serializedTransaction")
+            return serializedTransaction
+        } else {
+            Log.i("Coin", "Coin: ! the transaction could not be found in your wallet.")
+            return null
+        }
+    }
+
     companion object {
         fun createMultiSignatureWallet(
             publicKeys: List<ECKey>,
+            entranceFee: Coin,
             threshold: Int,
             params: NetworkParameters = MainNetParams.get()
         ): Transaction {
@@ -110,9 +174,8 @@ class WalletManager(
             // Create a n-n multi-signature output script.
             val script = ScriptBuilder.createMultiSigOutputScript(threshold, keys)
 
-            // Now add an output with minimum fee needed.
-            val amount: Coin = Transaction.REFERENCE_DEFAULT_MIN_TX_FEE
-            contract.addOutput(amount, script)
+            // Now add an output with the entrance fee & script.
+            contract.addOutput(entranceFee, script)
 
             return contract
         }
@@ -120,23 +183,29 @@ class WalletManager(
         fun signMultiSignatureMessage(
             contract: Transaction,
             myPublicKey: ECKey,
-            receiverAddress: ECKey,
+            receiverAddress: Address,
             value: Coin,
-            params: NetworkParameters = MainNetParams.get()
+            params: NetworkParameters
         ): ECDSASignature {
             // Retrieve the multisignature contract.
-            val multisigOutput: TransactionOutput = contract.getOutput(0)
+            var multisigOutput: TransactionOutput = contract.getOutput(0)
+            for (output in contract.outputs) {
+                if (ScriptPattern.isSentToMultisig(output.scriptPubKey)) {
+                    multisigOutput = output
+                }
+            }
             val multisigScript: Script = multisigOutput.scriptPubKey
 
             // Validate whether the transaction (= contract) is what we expect.
             if (!ScriptPattern.isSentToMultisig(multisigScript)) {
-                throw Exception("Contract is not a multi signature contract!")
+                Log.i(
+                    "Coin",
+                    "Coin: signMultiSignatureMessage, failing because we have not found a proper output."
+                )
+                throw Exception("Contract is not a multi-sig contract!")
             }
 
             // Build the transaction we want to sign.
-            // todo: add validation to check for this value
-            // todo: add fees (so we get chosen earlier by miners)
-            val value: Coin = value
             val spendTx = Transaction(params)
             spendTx.addOutput(value, receiverAddress)
             spendTx.addInput(multisigOutput)
--- demo-android/src/test/java/nl/tudelft/ipv8/android/demo/coin/WalletManagerTest.kt
+++ demo-android/src/test/java/nl/tudelft/ipv8/android/demo/coin/WalletManagerTest.kt
@@ -1,14 +1,13 @@
 package nl.tudelft.ipv8.android.demo.coin
 
-import org.bitcoinj.core.ECKey
-import org.bitcoinj.core.Transaction
-import org.bitcoinj.script.Script
-import org.junit.BeforeClass
 import nl.tudelft.ipv8.util.hexToBytes
 import org.bitcoinj.core.*
 import org.bitcoinj.params.MainNetParams
+import org.bitcoinj.params.TestNet3Params
+import org.bitcoinj.script.Script
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
+import org.junit.BeforeClass
 import org.junit.Test
 import java.io.File
 
@@ -31,7 +30,6 @@ class WalletManagerTest {
 
     @Test
     fun testCreateMultisignatureWallet() {
-        walletManager.printWalletInfo()
         val ECKey_1: ECKey =
             WalletManager.privateKeyStringToECKey("5KffUB9YUsvoGjrcn76PjVnC61PcWLzws4QPfrT9RFNd85utCkZ")
         val ECKey_2: ECKey =
@@ -45,7 +43,12 @@ class WalletManagerTest {
         println(ECKey_3.privateKeyAsHex)
 
         val contract: Transaction =
-            WalletManager.createMultiSignatureWallet(listOf(ECKey_1, ECKey_2, ECKey_3), 2)
+            WalletManager.createMultiSignatureWallet(
+                listOf(ECKey_1, ECKey_2, ECKey_3),
+                Coin.ZERO,
+                2,
+                TestNet3Params.get()
+            )
 
         val scriptPubKey: Script = contract.outputs[0].scriptPubKey
         val scriptPubKeyBytes: ByteArray = contract.outputs[0].scriptBytes
@@ -56,13 +59,22 @@ class WalletManagerTest {
     fun testMultiSigFromECDSA() {
         val params = MainNetParams.get()
 
-        val key1: ECKey = ecdsaPubKeyToECKey(params, "04A97B658C114D77DC5F71736AB78FBE408CE632ED1478D7EAA106EEF67C55D58A91C6449DE4858FAF11721E85FE09EC850C6578432EB4BE9A69C76232AC593C3B")
-        val key2: ECKey = ecdsaPubKeyToECKey(params, "04019EF04A316792F0ECBE5AB1718C833C3964DEE3626CFABE19D97745DBCAA5198919081B456E8EEEA5898AFA0E36D5C17AB693A80D728721128ED8C5F38CDBA0")
-        val key3: ECKey = ecdsaPubKeyToECKey(params, "04A04F29F308160E6F945B33D943304B1B471ED8F9EACEEB5412C04E60A0FAB0376871D9D1108948B67CAFBC703E565A18F8351FB8558FD7C7482D7027EECD687C")
+        val key1: ECKey = ecdsaPubKeyToECKey(
+            params,
+            "04A97B658C114D77DC5F71736AB78FBE408CE632ED1478D7EAA106EEF67C55D58A91C6449DE4858FAF11721E85FE09EC850C6578432EB4BE9A69C76232AC593C3B"
+        )
+        val key2: ECKey = ecdsaPubKeyToECKey(
+            params,
+            "04019EF04A316792F0ECBE5AB1718C833C3964DEE3626CFABE19D97745DBCAA5198919081B456E8EEEA5898AFA0E36D5C17AB693A80D728721128ED8C5F38CDBA0"
+        )
+        val key3: ECKey = ecdsaPubKeyToECKey(
+            params,
+            "04A04F29F308160E6F945B33D943304B1B471ED8F9EACEEB5412C04E60A0FAB0376871D9D1108948B67CAFBC703E565A18F8351FB8558FD7C7482D7027EECD687C"
+        )
 
         val keys = listOf(key1, key2, key3)
 
-        val contract = WalletManager.createMultiSignatureWallet(keys, 2)
+        val contract = WalletManager.createMultiSignatureWallet(keys, Coin.ZERO, 2, params)
 
         println("Inputs:")
         for (input in contract.inputs) {
@@ -79,12 +91,20 @@ class WalletManagerTest {
         val params = MainNetParams.get()
 
         // Transaction from exchange to our wallet for testing, invalid, since the input uses witnesses to send money, instead of a one-on-one transaction
-        val userBitcoinPk = LegacyAddress.fromString(params, "bc1qxtk5uxp2ka68za6jxzz7e8xnq5wh5tqxd02fx4")
-        val transactionHash = Sha256Hash.wrap("a7e93dc1c53d45dbd3bb7f342991c068065059e33fceedf6a3fc0887bd5a056a".hexToBytes())
-        val sharedWalletBitcoinPk = LegacyAddress.fromString(params, "1K6BWqtaXpDiZ4PEiPR3pNyXb4ZveKRWqm")
+        val userBitcoinPk =
+            LegacyAddress.fromString(params, "bc1qxtk5uxp2ka68za6jxzz7e8xnq5wh5tqxd02fx4")
+        val transactionHash =
+            Sha256Hash.wrap("a7e93dc1c53d45dbd3bb7f342991c068065059e33fceedf6a3fc0887bd5a056a".hexToBytes())
+        val sharedWalletBitcoinPk =
+            LegacyAddress.fromString(params, "1K6BWqtaXpDiZ4PEiPR3pNyXb4ZveKRWqm")
         val entranceFee = 0.01831933
 
-        val entranceFeePayed = WalletManager.checkEntranceFeeTransaction(userBitcoinPk, transactionHash, sharedWalletBitcoinPk, entranceFee)
+        val entranceFeePayed = WalletManager.checkEntranceFeeTransaction(
+            userBitcoinPk,
+            transactionHash,
+            sharedWalletBitcoinPk,
+            entranceFee
+        )
 
         assertFalse("The entrance fee should be payed", entranceFeePayed)
     }
@@ -94,12 +114,20 @@ class WalletManagerTest {
         val params = MainNetParams.get()
 
         // Transaction that does not exist
-        val userBitcoinPk = LegacyAddress.fromString(params, "bc1qxtk5uxp2ka68za6jxzz7e8xnq5wh5tqxd02fx4")
-        val transactionHash = Sha256Hash.wrap("a7e93dc1c53d45ddd3bb7f342991c068065059e33fceedf6a3fc0887bd5a056a".hexToBytes())
-        val sharedWalletBitcoinPk = LegacyAddress.fromString(params, "1K6BWqtaXpDiZ4PEiPR3pNyXb4ZveKRWqm")
+        val userBitcoinPk =
+            LegacyAddress.fromString(params, "bc1qxtk5uxp2ka68za6jxzz7e8xnq5wh5tqxd02fx4")
+        val transactionHash =
+            Sha256Hash.wrap("a7e93dc1c53d45ddd3bb7f342991c068065059e33fceedf6a3fc0887bd5a056a".hexToBytes())
+        val sharedWalletBitcoinPk =
+            LegacyAddress.fromString(params, "1K6BWqtaXpDiZ4PEiPR3pNyXb4ZveKRWqm")
         val entranceFee = 0.01831933
 
-        val entranceFeePayed = WalletManager.checkEntranceFeeTransaction(userBitcoinPk, transactionHash, sharedWalletBitcoinPk, entranceFee)
+        val entranceFeePayed = WalletManager.checkEntranceFeeTransaction(
+            userBitcoinPk,
+            transactionHash,
+            sharedWalletBitcoinPk,
+            entranceFee
+        )
 
         assertFalse("The entrance fee should be payed", entranceFeePayed)
     }
@@ -112,11 +140,18 @@ class WalletManagerTest {
         // Transaction from our wallet to another wallet for testing
         // https://www.blockchain.com/btc/tx/c0f8fdb122ac1f2db46652ac39afa0dd0ef1d9457eff684dc7ac5699ebc34a19
         val userBitcoinPk = LegacyAddress.fromString(params, "1K6BWqtaXpDiZ4PEiPR3pNyXb4ZveKRWqm")
-        val transactionHash = Sha256Hash.wrap("c0f8fdb122ac1f2db46652ac39afa0dd0ef1d9457eff684dc7ac5699ebc34a19".hexToBytes())
-        val sharedWalletBitcoinPk = LegacyAddress.fromString(params, "1BoCA9Y3z7NjQuFffdjKPo4PghWGRjsR5Y")
+        val transactionHash =
+            Sha256Hash.wrap("c0f8fdb122ac1f2db46652ac39afa0dd0ef1d9457eff684dc7ac5699ebc34a19".hexToBytes())
+        val sharedWalletBitcoinPk =
+            LegacyAddress.fromString(params, "1BoCA9Y3z7NjQuFffdjKPo4PghWGRjsR5Y")
         val entranceFee = 0.00600000
 
-        val entranceFeePayed = WalletManager.checkEntranceFeeTransaction(userBitcoinPk, transactionHash, sharedWalletBitcoinPk, entranceFee)
+        val entranceFeePayed = WalletManager.checkEntranceFeeTransaction(
+            userBitcoinPk,
+            transactionHash,
+            sharedWalletBitcoinPk,
+            entranceFee
+        )
 
         assertTrue("The entrance fee should be payed", entranceFeePayed)
     }
@@ -128,11 +163,18 @@ class WalletManagerTest {
         // Confirmed transaction found on blockchain
         // https://www.blockchain.com/btc/tx/3c5771e1c8e40056ef04d96dade01d707fade25c3847f98c93f03715accd4414
         val userBitcoinPk = LegacyAddress.fromString(params, "38tQTweJNJFSv6xM5Zb39ZWqzikk81fMdy")
-        val transactionHash = Sha256Hash.wrap("3c5771e1c8e40056ef04d96dade01d707fade25c3847f98c93f03715accd4414".hexToBytes())
-        val sharedWalletBitcoinPk = LegacyAddress.fromString(params, "1CbpHMm5AXRVTuXXZ7rL4wShcRKosdCQPZ")
+        val transactionHash =
+            Sha256Hash.wrap("3c5771e1c8e40056ef04d96dade01d707fade25c3847f98c93f03715accd4414".hexToBytes())
+        val sharedWalletBitcoinPk =
+            LegacyAddress.fromString(params, "1CbpHMm5AXRVTuXXZ7rL4wShcRKosdCQPZ")
         val entranceFee = 1.0
 
-        val entranceFeePayed = WalletManager.checkEntranceFeeTransaction(userBitcoinPk, transactionHash, sharedWalletBitcoinPk, entranceFee)
+        val entranceFeePayed = WalletManager.checkEntranceFeeTransaction(
+            userBitcoinPk,
+            transactionHash,
+            sharedWalletBitcoinPk,
+            entranceFee
+        )
 
         assertTrue("The entrance fee should be payed", entranceFeePayed)
     }
