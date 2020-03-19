0542b04602898c6be1fa86c14f82e80432611ff2
InvictusRMC <rowdy_chotkan@msn.com>
2020-03-19 15:00:58 +0100

    Remove unused file + imports + fix typos

--- demo-android/src/main/java/nl/tudelft/ipv8/android/demo/ui/bitcoin/SharedWalletTransaction.kt
+++ demo-android/src/main/java/nl/tudelft/ipv8/android/demo/ui/bitcoin/SharedWalletTransaction.kt
@@ -36,11 +36,8 @@ class SharedWalletTransaction : Fragment() {
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment SharedWalletTransaction.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance() = SharedWalletTransaction()
     }
