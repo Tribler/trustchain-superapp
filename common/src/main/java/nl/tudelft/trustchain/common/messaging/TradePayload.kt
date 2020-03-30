790646820ebc120da310c144a45d21b641ca8814
Erik Dymbe <erik.dymbe@gmail.com>
2020-03-30 12:05:12 +0200

    Add public key to TradePayload

--- common/src/main/java/nl/tudelft/trustchain/common/messaging/TradePayload.kt
+++ common/src/main/java/nl/tudelft/trustchain/common/messaging/TradePayload.kt
@@ -4,6 +4,7 @@ import nl.tudelft.ipv8.messaging.*
 import nl.tudelft.trustchain.common.constants.Currency
 
 class TradePayload(
+    val publicKey: ByteArray,        //
     val primaryCurrency: Currency,   // Currency to be sold in an ask, to buy in a bid
     val secondaryCurrency: Currency, // Currency to be paid in in an ask, to pay with in a bid
     val amount: Double,              // Amount of primary currency
@@ -11,7 +12,8 @@ class TradePayload(
     val type: Type                   // Type of message
 ) : Serializable {
     override fun serialize(): ByteArray {
-        return serializeVarLen(primaryCurrency.toString().toByteArray(Charsets.UTF_8)) +
+        return serializeVarLen(publicKey) +
+            serializeVarLen(primaryCurrency.toString().toByteArray(Charsets.UTF_8)) +
             serializeVarLen(secondaryCurrency.toString().toByteArray(Charsets.UTF_8)) +
             serializeVarLen(amount.toString().toByteArray(Charsets.UTF_8)) +
             serializeVarLen(price.toString().toByteArray(Charsets.UTF_8)) +
@@ -21,6 +23,8 @@ class TradePayload(
     companion object Deserializer : Deserializable<TradePayload> {
         override fun deserialize(buffer: ByteArray, offset: Int): Pair<TradePayload, Int> {
             var localOffset = 0
+            val (publicKey, publicKeySize) = deserializeVarLen(buffer, offset + localOffset)
+            localOffset += publicKeySize
             val (askCurrency, askCurrencySize) = deserializeVarLen(buffer, offset + localOffset)
             localOffset += askCurrencySize
             val (paymentCurrency, paymentCurrencySize) = deserializeVarLen(buffer, offset + localOffset)
@@ -32,6 +36,7 @@ class TradePayload(
             val (type, typeSize) = deserializeVarLen(buffer, offset + localOffset)
             localOffset += typeSize
             val payload = TradePayload(
+                publicKey,
                 Currency.valueOf(askCurrency.toString(Charsets.UTF_8)),
                 Currency.valueOf(paymentCurrency.toString(Charsets.UTF_8)),
                 amount.toString(Charsets.UTF_8).toDouble(),
