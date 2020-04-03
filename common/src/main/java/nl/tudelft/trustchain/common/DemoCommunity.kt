9dbce6a0286f64c6a9d29331b9971ed152d44209
tpapak01 <tpapak01@cs.ucy.ac.cy>
2020-04-03 20:46:57 +0200

    --

--- common/src/main/java/nl/tudelft/trustchain/common/DemoCommunity.kt
+++ common/src/main/java/nl/tudelft/trustchain/common/DemoCommunity.kt
@@ -1,29 +1,19 @@
 package nl.tudelft.trustchain.common
 
-import android.content.Context
 import android.util.Log
-import android.widget.Toast
 import nl.tudelft.ipv8.IPv4Address
 import nl.tudelft.ipv8.Community
 import nl.tudelft.ipv8.Peer
 import nl.tudelft.ipv8.messaging.*
 import nl.tudelft.ipv8.messaging.payload.IntroductionResponsePayload
 import java.util.*
-import kotlin.coroutines.coroutineContext
 
 class DemoCommunity() : Community() {
-
     override val serviceId = "02313685c1912a141279f8248fc8db5899c5df5a"
 
     val discoveredAddressesContacted: MutableMap<IPv4Address, Date> = mutableMapOf()
 
     val lastTrackerResponses = mutableMapOf<IPv4Address, Date>()
-
-    //var myContext : Context? = null;
-
-    //fun giveContext(context: Context?){
-    //    myContext = context
-    //}
 
     override fun walkTo(address: IPv4Address) {
         super.walkTo(address)
