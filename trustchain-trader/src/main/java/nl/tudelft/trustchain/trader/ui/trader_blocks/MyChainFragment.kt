b98cf610e001df940be2de3923e970670a64403b
Martijn van Meerten <martijnvanmeerten@hotmail.com>
2020-04-03 18:40:21 +0200

    Rename modules

--- trustchain-trader/src/main/java/nl/tudelft/trustchain/trader/ui/trader_blocks/MyChainFragment.kt
+++
@@ -1,9 +0,0 @@
-package nl.tudelft.trustchain.trader.ui.trader_blocks
-
-class MyChainFragment : MarketBlocksFragment() {
-    override val isCrawlAllowed = false
-
-    override fun getPublicKey(): ByteArray {
-        return getTrustChainCommunity().myPeer.publicKey.keyToBin()
-    }
-}
