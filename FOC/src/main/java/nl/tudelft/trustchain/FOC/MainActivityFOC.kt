3268c6f000d5842032c9bd5ca6e21c1098e96d38
tpapak01 <tpapak01@cs.ucy.ac.cy>
2020-04-03 19:53:10 +0200

    Temp commit

--- FOC/src/main/java/nl/tudelft/trustchain/FOC/MainActivityFOC.kt
+++ FOC/src/main/java/nl/tudelft/trustchain/FOC/MainActivityFOC.kt
@@ -1,6 +1,7 @@
 package nl.tudelft.trustchain.FOC
 
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +14,9 @@ import kotlinx.android.synthetic.main.activity_main_foc.*
 import kotlinx.android.synthetic.main.content_main_activity_foc.*
 import nl.tudelft.ipv8.Overlay
 import java.io.File
+import java.io.IOException
+import java.io.RandomAccessFile
+import java.nio.channels.FileChannel
 import java.util.*
 import java.util.concurrent.CountDownLatch
 import kotlin.math.roundToInt
@@ -35,16 +39,21 @@ class MainActivityFOC : AppCompatActivity() {
         //        .setAction("Action", null).show()
         //}
 
-        greetPeersButton.setOnClickListener { view ->
+        downloadMagnet.setOnClickListener { view ->
             //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
             //    .setAction("Action", null).show()
             //Toast.makeText(nl.tudelft.trustchain.common., "No magnet link provided, using default one...", Toast.LENGTH_LONG).show()
-            getMagnet()
-
-
+            getMagnetLink()
         }
 
         downloadTorrent.setOnClickListener { view ->
+            //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            //    .setAction("Action", null).show()
+            //Toast.makeText(nl.tudelft.trustchain.common., "No magnet link provided, using default one...", Toast.LENGTH_LONG).show()
+            getTorrent()
+        }
+
+        uploadTorrent.setOnClickListener { view ->
             Toast.makeText(applicationContext, "No magnet again", Toast.LENGTH_LONG).show()
         }
 
@@ -81,10 +90,12 @@ class MainActivityFOC : AppCompatActivity() {
         Toast.makeText(applicationContext, s, Toast.LENGTH_LONG).show()
     }
 
-    fun getMagnet(){
+    fun getMagnetLink(){
         var magnetLink: String? = null
         val inputText = enterTorrent.text.toString()
-        if (inputText == "") { //String uri = "magnet:?xt=urn:btih:86d0502ead28e495c9e67665340f72aa72fe304e&dn=Frostwire.5.3.6.+%5BWindows%5D&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.istole.it%3A6969&tr=udp%3A%2F%2Fopen.demonii.com%3A1337";
+        if (inputText == "") {
+            printToast("No magnet link given, using default")
+            //magnetLink = "magnet:?xt=urn:btih:86d0502ead28e495c9e67665340f72aa72fe304e&dn=Frostwire.5.3.6.+%5BWindows%5D&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.istole.it%3A6969&tr=udp%3A%2F%2Fopen.demonii.com%3A1337";
             //magnetLink = "magnet:?xt=urn:btih:737d38ed01da1df727a3e0521a6f2c457cb812de&dn=HOME+-+a+film+by+Yann+Arthus-Bertrand+%282009%29+%5BEnglish%5D+%5BHD+MP4%5D&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969";
             //magnetLink = "magnet:?xt=urn:btih:a83cc13bf4a07e85b938dcf06aa707955687ca7c";
             magnetLink = "magnet:?xt=urn:btih:209c8226b299b308beaf2b9cd3fb49212dbd13ec&dn=Tears+of+Steel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Ftears-of-steel.torrent"
@@ -171,6 +182,90 @@ class MainActivityFOC : AppCompatActivity() {
             printToast("Something went wrong, check logs")
         }
 
+    }
+
+    fun getTorrent(){
+
+        val s = SessionManager()
+
+        val sp = SettingsPack()
+
+        val params =
+            SessionParams(sp)
+
+        val signal = CountDownLatch(1)
+
+        s.addListener(object : AlertListener {
+            override fun types(): IntArray? {
+                return null
+            }
+
+            override fun alert(alert: Alert<*>) {
+                val type = alert.type()
+
+                when (type) {
+                    AlertType.ADD_TORRENT -> {
+                        Log.i("personal", "Torrent added")
+                        //System.out.println("Torrent added");
+                        (alert as AddTorrentAlert).handle().resume()
+                    }
+                    AlertType.BLOCK_FINISHED -> {
+                        val a = alert as BlockFinishedAlert
+                        val p = (a.handle().status().progress() * 100).toInt()
+                        progressBar.setProgress(p, true)
+                        Log.i(
+                            "personal",
+                            "Progress: " + p + " for torrent name: " + a.torrentName()
+                        )
+                        Log.i(
+                            "personal",
+                            java.lang.Long.toString(s.stats().totalDownload())
+                        )
+                    }
+                    AlertType.TORRENT_FINISHED -> {
+                        Log.i("personal", "Torrent finished")
+                        printToast("Torrent downloaded!!")
+                        signal.countDown()
+                    }
+                }
+
+            }
+
+        });
+
+        s.start(params)
+
+        val torrent =
+            Environment.getExternalStorageDirectory().absolutePath + "/sintel.torrent"
+        try {
+            readTorrent(torrent)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        printToast("Starting download, please wait...")
+
+        val torrentFile = File(torrent)
+        val ti = TorrentInfo(torrentFile)
+        Log.i(
+            "personal",
+            "Storage of downloads: " + torrentFile.parentFile.toString()
+        )
+        s.download(ti, torrentFile.parentFile)
+    }
+
+    @Throws(IOException::class)
+    fun readTorrent(torrent: String?) {
+        val torrentFile = File(torrent)
+        val ti = TorrentInfo(torrentFile)
+        val fc = RandomAccessFile(torrent, "r").channel
+        val buffer =
+            fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size())
+        val ti2 = TorrentInfo(buffer)
+        var toPrint = ti.toEntry().toString() + ti2.toEntry().toString()
+        Log.i("personal", ti.toEntry().toString())
+        Log.i("personal", ti2.toEntry().toString())
+        torrentView.text = toPrint
     }
 
 }
