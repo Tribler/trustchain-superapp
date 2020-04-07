c308051680440e5e961810e9ee307c36c2376be6
Paulvanderlaan <P.J.vanderlaan-1@student.tudelft.nl>
2020-04-07 15:14:14 +0200

    Fix linting issues and stop ignoring failures

--- currencyii/src/main/java/nl/tudelft/trustchain/currencyii/ui/bitcoin/DaoLoginChoice.kt
+++ currencyii/src/main/java/nl/tudelft/trustchain/currencyii/ui/bitcoin/DaoLoginChoice.kt
@@ -41,7 +41,8 @@ class DaoLoginChoice : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
