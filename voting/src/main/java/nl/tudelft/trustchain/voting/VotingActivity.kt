03dee8a5543133fe96c0135837ec361a6a56dac1
EmieldeSmidt <desmidt.emiel@gmail.com>
2020-04-14 21:24:12 +0200

    cleanup

--- voting/src/main/java/nl/tudelft/trustchain/voting/VotingActivity.kt
+++ voting/src/main/java/nl/tudelft/trustchain/voting/VotingActivity.kt
@@ -9,13 +9,15 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main_voting.*
 import nl.tudelft.ipv8.android.IPv8Android
 import nl.tudelft.ipv8.attestation.trustchain.TrustChainCommunity
+import nl.tudelft.ipv8.keyvault.PublicKey
 import nl.tudelft.trustchain.common.util.VotingHelper
-import java.security.PublicKey
 
 class VotingActivity : AppCompatActivity() {
 
     lateinit var vh: VotingHelper
     lateinit var community: TrustChainCommunity
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_voting)
@@ -24,44 +26,10 @@ class VotingActivity : AppCompatActivity() {
             showNewVoteDialog()
         }
 
-
         val ipv8 = IPv8Android.getInstance()
-        community = ipv8.getOverlay<TrustChainCommunity>()!!
+        community = ipv8.getOverlay()!!
         vh = VotingHelper(community)
 
-//
-//        // option 1: download a torrent through a magnet link
-//        downloadMagnetButton.setOnClickListener { _ ->
-//            getMagnetLink()
-//        }
-//
-//        // option 2: download a torrent through a .torrent file on your phone
-//        downloadTorrentButton.setOnClickListener { _ ->
-//            getTorrent()
-//        }
-//
-//        // option 3: Send a message to every other peer using the superapp
-//        greetPeersButton.setOnClickListener { _ ->
-//            val ipv8 = IPv8Android.getInstance()
-//            val demoCommunity = ipv8.getOverlay<DemoCommunity>()!!
-//            val peers = demoCommunity.getPeers()
-//
-//            Log.i("personal", "n:" + peers.size.toString())
-//            for (peer in peers) {
-//                Log.i("personal", peer.mid)
-//            }
-//
-//            demoCommunity.broadcastGreeting()
-//            printToast("Greeted " + peers.size.toString() + " peers")
-//        }
-//
-//        // option 4: dynamically load and execute code from a jar/apk file
-//        executeCodeButton.setOnClickListener { _ ->
-//            loadDynamicCode()
-//        }
-//
-//        // upon launching our activity, we ask for the "Storage" permission
-//        requestStoragePermission()
     }
 
     /**
@@ -87,7 +55,7 @@ class VotingActivity : AppCompatActivity() {
             val proposal = input.text.toString()
 
             // Create list of your peers and include yourself
-            val peers: MutableList<nl.tudelft.ipv8.keyvault.PublicKey> = ArrayList()
+            val peers: MutableList<PublicKey> = ArrayList()
             peers.addAll(community.getPeers().map { it.publicKey })
             peers.add(community.myPeer.publicKey)
 
