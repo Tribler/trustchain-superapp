a7e4ba8fbe58adce2383b80e2a39b2188ea232dc
Chris Lemaire <c.lemaire@student.tudelft.nl>
2021-02-15 12:00:31 +0100

    Setup a starter Liquidity Pool project
    This liquidity pool project is so far just a plain copy
    of the musicdao project with the name replaced with Liquidity
    Pool.
    
    We will further build on this module in the future.

---
+++ liquidity-pool/src/main/java/nl/tudelft/trustchain/liquidity/LiquidityPoolMainActivity.kt
@@ -0,0 +1,8 @@
+package nl.tudelft.trustchain.liquidity
+
+import nl.tudelft.trustchain.common.BaseActivity
+
+class LiquidityPoolMainActivity : BaseActivity() {
+
+    override val navigationGraph = R.navigation.nav_graph_liquidity
+}
