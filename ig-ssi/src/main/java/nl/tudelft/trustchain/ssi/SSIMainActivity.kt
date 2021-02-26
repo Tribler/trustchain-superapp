1cd19a9a3858e8d58b0c871e4abff7f2c03fdc9e
Rowdy Chotkan <rowdy_chotkan@msn.com>
2021-02-26 12:28:00 +0100

    Add attestation chunk callback

--- ig-ssi/src/main/java/nl/tudelft/trustchain/ssi/SSIMainActivity.kt
+++ ig-ssi/src/main/java/nl/tudelft/trustchain/ssi/SSIMainActivity.kt
@@ -34,6 +34,22 @@ class SSIMainActivity : BaseActivity() {
         val community = IPv8Android.getInstance().getOverlay<AttestationCommunity>()!!
         community.setAttestationRequestCallback(::attestationRequestCallback)
         community.setAttestationRequestCompleteCallback(::attestationRequestCompleteCallback)
+        community.setAttestationChunkCallback(::attestationChunkCallback)
+
+        // Register own key as trusted authority.
+        community.trustedAuthorityManager.addTrustedAuthority(IPv8Android.getInstance().myPeer.publicKey)
+    }
+
+    private fun attestationChunkCallback(peer: Peer, i: Int) {
+        logger.info("Received attestation chunk $i from ${peer.mid}.")
+        Handler(Looper.getMainLooper()).post {
+            Toast.makeText(
+                applicationContext,
+                "Received attestation chunk $i from ${peer.mid}.",
+                Toast.LENGTH_LONG
+            )
+                .show()
+        }
     }
 
     @Suppress("UNUSED_PARAMETER")
