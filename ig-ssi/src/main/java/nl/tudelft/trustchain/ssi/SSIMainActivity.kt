7e92e3f3efeb27b31282e7fd938d6b1a49d8e521
Rowdy Chotkan <rowdy_chotkan@msn.com>
2021-03-02 13:41:42 +0100

    Move dialogs to seperate classes + add default attestation complete callback

--- ig-ssi/src/main/java/nl/tudelft/trustchain/ssi/SSIMainActivity.kt
+++ ig-ssi/src/main/java/nl/tudelft/trustchain/ssi/SSIMainActivity.kt
@@ -1,42 +1,27 @@
 package nl.tudelft.trustchain.ssi
 
-import android.annotation.SuppressLint
-import android.app.Dialog
-import android.content.DialogInterface
-import android.graphics.Bitmap
+import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.text.Html
-import android.text.InputType
-import android.view.View
+import android.util.Log
 import android.widget.*
-import androidx.appcompat.app.AlertDialog
-import androidx.core.view.isVisible
-import androidx.fragment.app.DialogFragment
-import com.google.android.material.textfield.TextInputEditText
-import com.jaredrummler.blockingdialog.BlockingDialogFragment
 import com.jaredrummler.blockingdialog.BlockingDialogManager
-import mu.KotlinLogging
-import nl.tudelft.ipv8.IPv4Address
 import nl.tudelft.ipv8.Peer
 import nl.tudelft.ipv8.android.IPv8Android
 import nl.tudelft.ipv8.attestation.WalletAttestation
 import nl.tudelft.ipv8.attestation.schema.*
-import nl.tudelft.ipv8.attestation.wallet.AttestationBlob
 import nl.tudelft.ipv8.attestation.wallet.AttestationCommunity
 import nl.tudelft.trustchain.common.BaseActivity
+import nl.tudelft.trustchain.ssi.dialogs.AttestationValueDialog
 import org.json.JSONObject
-import java.util.*
-
-private val logger = KotlinLogging.logger {}
 
 class SSIMainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val community = IPv8Android.getInstance().getOverlay<AttestationCommunity>()!!
         community.setAttestationRequestCallback(::attestationRequestCallback)
-        community.setAttestationRequestCompleteCallback(::attestationRequestCompleteCallback)
+        community.setAttestationRequestCompleteCallback(::attestationRequestCompleteCallbackWrapper)
         community.setAttestationChunkCallback(::attestationChunkCallback)
 
         // Register own key as trusted authority.
@@ -44,7 +29,7 @@ class SSIMainActivity : BaseActivity() {
     }
 
     private fun attestationChunkCallback(peer: Peer, i: Int) {
-        logger.info("Received attestation chunk $i from ${peer.mid}.")
+        Log.i("ig-ssi", "Received attestation chunk $i from ${peer.mid}.")
         Handler(Looper.getMainLooper()).post {
             Toast.makeText(
                 applicationContext,
@@ -55,8 +40,8 @@ class SSIMainActivity : BaseActivity() {
         }
     }
 
-    @Suppress("UNUSED_PARAMETER")
-    private fun attestationRequestCompleteCallback(
+    // Default callback, currently overwritten in DatabaseFragment.
+    private fun attestationRequestCompleteCallbackWrapper(
         forPeer: Peer,
         attributeName: String,
         attestation: WalletAttestation,
@@ -64,29 +49,19 @@ class SSIMainActivity : BaseActivity() {
         idFormat: String,
         fromPeer: Peer?,
         metaData: String?,
-        Signature: ByteArray?
+        signature: ByteArray?
     ) {
-        if (fromPeer == null) {
-            logger.info("Signed attestation for attribute $attributeName for peer ${forPeer.mid}.")
-            Handler(Looper.getMainLooper()).post {
-                Toast.makeText(
-                    applicationContext,
-                    "Successfully sent attestation for $attributeName to peer ${forPeer.mid}",
-                    Toast.LENGTH_LONG
-                )
-                    .show()
-            }
-        } else {
-            logger.info("Received attestation for attribute $attributeName with metadata: $metaData.")
-            Handler(Looper.getMainLooper()).post {
-                Toast.makeText(
-                    applicationContext,
-                    "Received Attestation for $attributeName",
-                    Toast.LENGTH_LONG
-                )
-                    .show()
-            }
-        }
+        attestationRequestCompleteCallback(
+            forPeer,
+            attributeName,
+            attestation,
+            attributeHash,
+            idFormat,
+            fromPeer,
+            metaData,
+            signature,
+            applicationContext
+        )
     }
 
     @Suppress("UNUSED_PARAMETER")
@@ -95,14 +70,14 @@ class SSIMainActivity : BaseActivity() {
         attributeName: String,
         metadata: String
     ): ByteArray {
-        logger.info("Attestation: called")
+        Log.i("ig-ssi", "Attestation: called")
         val parsedMetadata = JSONObject(metadata)
         val idFormat = parsedMetadata.optString("id_format", ID_METADATA)
         val input =
             BlockingDialogManager.getInstance()
                 .showAndWait<String?>(this, AttestationValueDialog(attributeName, idFormat))
                 ?: throw RuntimeException("User cancelled dialog.")
-        logger.info("Signing attestation with value $input with format $idFormat.")
+        Log.i("ig-ssi", "Signing attestation with value $input with format $idFormat.")
         Handler(Looper.getMainLooper()).post {
             Toast.makeText(
                 applicationContext,
@@ -121,227 +96,43 @@ class SSIMainActivity : BaseActivity() {
     override val bottomNavigationMenu = R.menu.bottom_navigation_menu2
 }
 
-class FireMissilesDialogFragment(val peer: Peer) : DialogFragment() {
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return activity?.let {
-            // Use the Builder class for convenient dialog construction
-            val builder = AlertDialog.Builder(it)
-            val inflater = requireActivity().layoutInflater
-            val attestationCommunity =
-                IPv8Android.getInstance().getOverlay<AttestationCommunity>()!!
-            val view = inflater.inflate(R.layout.request_attestation_dialog, null)
-            val spinner = view.findViewById<Spinner>(R.id.idFormatSpinner)
-            val arrayAdapter = ArrayAdapter(
-                requireContext(),
-                R.layout.support_simple_spinner_dropdown_item,
-                attestationCommunity.schemaManager.getSchemaNames().sorted()
+@Suppress("UNUSED_PARAMETER")
+fun attestationRequestCompleteCallback(
+    forPeer: Peer,
+    attributeName: String,
+    attestation: WalletAttestation,
+    attributeHash: ByteArray,
+    idFormat: String,
+    fromPeer: Peer?,
+    metaData: String?,
+    signature: ByteArray?,
+    context: Context
+) {
+    if (fromPeer == null) {
+        Log.i(
+            "ig-ssi",
+            "Signed attestation for attribute $attributeName for peer ${forPeer.mid}."
+        )
+        Handler(Looper.getMainLooper()).post {
+            Toast.makeText(
+                context,
+                "Successfully sent attestation for $attributeName to peer ${forPeer.mid}",
+                Toast.LENGTH_LONG
             )
-            arrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
-            spinner.adapter = arrayAdapter
-            spinner.setSelection(3, true)
-
-            builder.setView(view)
-                .setPositiveButton(
-                    R.string.fire,
-                    null
-                )
-                .setNegativeButton(
-                    R.string.cancel,
-                    DialogInterface.OnClickListener { _, _ -> }
-                )
-                .setTitle("Request Attestation")
-            // Create the AlertDialog object and return it
-            val dialog = builder.create()
-            dialog.setOnShowListener {
-                val posBtn = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
-                posBtn.setOnClickListener {
-                    val attrInput = view.findViewById<TextInputEditText>(R.id.attribute_input)
-                    if (attrInput.text.toString() != "") {
-                        val idFormat = spinner.selectedItem.toString()
-                        val myPeer = IPv8Android.getInstance().myPeer
-                        val key = when (idFormat) {
-                            ID_METADATA_BIG -> myPeer.identityPrivateKeyBig!!
-                            ID_METADATA_HUGE -> myPeer.identityPrivateKeyHuge!!
-                            else -> myPeer.identityPrivateKeySmall!!
-                        }
-                        attestationCommunity.requestAttestation(
-                            peer,
-                            attrInput.text.toString().toUpperCase(Locale.getDefault()),
-                            key,
-                            hashMapOf("id_format" to idFormat),
-                            true
-                        )
-                        logger.info("Sending attestation for ${attrInput.text} to ${peer.mid}")
-                        dialog.dismiss()
-                        Toast.makeText(
-                            requireContext(),
-                            "Requested attestation for ${attrInput.text} from ${peer.mid}",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    } else {
-                        attrInput.error = "Please enter a claim name."
-                    }
-                }
-            }
-            dialog
-        } ?: throw IllegalStateException("Activity cannot be null")
-    }
-}
-
-@SuppressLint("ValidFragment")
-class AttestationValueDialog(private val attributeName: String, private val idFormat: String) :
-    BlockingDialogFragment<String>() {
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): AlertDialog? {
-        @Suppress("DEPRECATION")
-        return activity?.let {
-            val builder = AlertDialog.Builder(it)
-            val inflater = it.layoutInflater
-            val view = inflater.inflate(R.layout.attestation_value_dialog, null)
-            val attrInput = view.findViewById<TextInputEditText>(R.id.value_input)
-            when (idFormat) {
-                ID_METADATA_RANGE_18PLUS -> attrInput.inputType = InputType.TYPE_CLASS_NUMBER
-                ID_METADATA_RANGE_UNDERAGE -> attrInput.inputType = InputType.TYPE_CLASS_NUMBER
-                else -> attrInput.inputType = InputType.TYPE_TEXT_FLAG_AUTO_COMPLETE
-            }
-
-            builder.setView(view)
-                .setPositiveButton(
-                    R.string.fire,
-                    null
-                )
-                .setNegativeButton(
-                    R.string.cancel,
-                    DialogInterface.OnClickListener { _, _ ->
-                        setResult("", true)
-                    }
-                )
-                .setTitle("Attestation Requested")
-                .setMessage(Html.fromHtml("An attestation has been requested for <b>$attributeName</b> with format <b>$idFormat</b>."))
-            // Create the AlertDialog object and return it
-            val dialog = builder.create()
-            dialog.setOnShowListener {
-                val posBtn = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
-                posBtn.setOnClickListener {
-                    val inputValue = attrInput.text.toString()
-                    if (inputValue != "") {
-                        setResult(inputValue, false)
-                        dialog.dismiss()
-                    } else {
-                        attrInput.error = "Enter a value."
-                    }
-                }
-            }
-            dialog
-        } ?: throw IllegalStateException("Activity cannot be null")
-    }
-}
-
-class PresentAttestationDialog(
-    private val attributeName: String,
-    private val attributeValue: String
-) :
-    DialogFragment() {
-
-    private var mView: View? = null
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return activity?.let {
-            val builder = AlertDialog.Builder(it)
-            val inflater = requireActivity().layoutInflater
-
-            @SuppressLint("InflateParams")
-            val view = inflater.inflate(R.layout.present_attestation_dialog, null)!!
-            mView = view
-            builder.setView(mView)
-            builder.setTitle("hello")
-
-            val dialog: Dialog
-            val title = "Attestation for <font color='#EE0000'>${attributeName.capitalize()}</font>"
-            val message = "<b>$attributeName:</b> $attributeValue"
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-                builder.setTitle(Html.fromHtml(title, Html.FROM_HTML_MODE_LEGACY))
-                dialog = builder.create()
-                dialog.setMessage(
-                    Html.fromHtml(message, Html.FROM_HTML_MODE_LEGACY)
-                )
-            } else {
-                @Suppress("DEPRECATION")
-                builder.setTitle(Html.fromHtml(title))
-                dialog = builder.create()
-                @Suppress("DEPRECATION")
-                dialog.setMessage(
-                    Html.fromHtml(message)
-                )
-            }
-            dialog
-        } ?: throw IllegalStateException("Activity cannot be null")
-    }
-
-    fun setQRCode(bitmap: Bitmap) {
-        val progressBar = mView!!.findViewById<ProgressBar>(R.id.progressBar)
-        if (progressBar.isVisible) {
-            progressBar.visibility = View.GONE
+                .show()
         }
-        mView!!.findViewById<ImageView>(R.id.qrCodeView).setImageBitmap(bitmap)
-    }
-}
-
-class VerifyAttestationDialog(val databaseBlob: AttestationBlob) : DialogFragment() {
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return activity?.let {
-            // Use the Builder class for convenient dialog construction
-            val builder = AlertDialog.Builder(it)
-            val inflater = requireActivity().layoutInflater
-            val attestationCommunity =
-                IPv8Android.getInstance().getOverlay<AttestationCommunity>()!!
-            val view = inflater.inflate(R.layout.verify_attestation_dialog, null)
-            builder.setView(view)
-                .setPositiveButton(
-                    R.string.fire,
-                    DialogInterface.OnClickListener { _, _ ->
-                        val addressInput =
-                            view.findViewById<TextInputEditText>(R.id.peer_address_input).text.toString()
-                        val attributeName =
-                            view.findViewById<TextInputEditText>(R.id.attribute_name_input).text.toString()
-                        val input = addressInput.split(":").toTypedArray()
-                        val ip = input[0]
-                        val port = input[1].toInt()
-                        var address: IPv4Address? = null
-                        for (peer in attestationCommunity.getPeers()) {
-                            if (peer.address.toString() == addressInput) {
-                                address = IPv4Address(ip, port)
-                            }
-                        }
-                        if (address == null) {
-                            throw RuntimeException("IPv4 Address not found")
-                        }
-                        logger.info("SII: Sending verify request")
-                        attestationCommunity.verifyAttestationValues(
-                            address,
-                            databaseBlob.attestationHash,
-                            arrayListOf(attributeName.toByteArray()),
-                            ::verifyComplete,
-                            databaseBlob.idFormat
-                        )
-                    }
-                )
-
-                .setNegativeButton(
-                    R.string.cancel,
-                    DialogInterface.OnClickListener { _, _ -> }
-                )
-                .setTitle("Request Attestation")
-            // Create the AlertDialog object and return it
-            builder.create()
-        } ?: throw IllegalStateException("Activity cannot be null")
-    }
-
-    private fun verifyComplete(hash: ByteArray, values: List<Double>) {
-        println(" *** VerifyComplete: $hash, values:")
-        values.forEach { print(" $it") }
-        println(" ***")
+    } else {
+        Log.i(
+            "ig-ssi",
+            "Received attestation for attribute $attributeName with metadata: $metaData."
+        )
+        Handler(Looper.getMainLooper()).post {
+            Toast.makeText(
+                context,
+                "Received Attestation for $attributeName",
+                Toast.LENGTH_LONG
+            )
+                .show()
+        }
     }
 }
