97a75a0e997f324ebaf2bb75713a5ed0cbed8ae8
stefan <s.weegink@student.tudelft.nl>
2021-03-02 14:31:36 +0100

    add api call locations

--- musicdao/src/main/java/com/example/musicdao/dialog/TipArtistDialog.kt
+++ musicdao/src/main/java/com/example/musicdao/dialog/TipArtistDialog.kt
@@ -33,6 +33,8 @@ class TipArtistDialog(private val publicKey: String) : DialogFragment() {
         val instructionText = dialogView?.findViewById<TextView>(R.id.instructionText)
         instructionText?.text = "Sending a tip to artist(s), with public key: $publicKey"
 
+        // Todo: Create proposal
+
         val amountEditText = dialogView?.findViewById<EditText>(R.id.amount)
         val walletDir = context?.cacheDir ?: throw Error("CacheDir not found")
         val walletService = WalletService.getInstance(walletDir, (activity as MusicService))
