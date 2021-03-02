2d9a30a0a6888529f241f98a39554965582fa3b7
Rowdy Chotkan <rowdy_chotkan@msn.com>
2021-03-02 13:43:08 +0100

    Move dialogs to seperate files

---
+++ ig-ssi/src/main/java/nl/tudelft/trustchain/ssi/dialogs/AttestationValueDialog.kt
@@ -0,0 +1,62 @@
+package nl.tudelft.trustchain.ssi.dialogs
+
+import android.annotation.SuppressLint
+import android.content.DialogInterface
+import android.os.Bundle
+import android.text.Html
+import android.text.InputType
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.textfield.TextInputEditText
+import com.jaredrummler.blockingdialog.BlockingDialogFragment
+import nl.tudelft.ipv8.attestation.schema.ID_METADATA_RANGE_18PLUS
+import nl.tudelft.ipv8.attestation.schema.ID_METADATA_RANGE_UNDERAGE
+import nl.tudelft.trustchain.ssi.R
+
+@SuppressLint("ValidFragment")
+class AttestationValueDialog(private val attributeName: String, private val idFormat: String) :
+    BlockingDialogFragment<String>() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): AlertDialog? {
+        @Suppress("DEPRECATION")
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            val inflater = it.layoutInflater
+            val view = inflater.inflate(R.layout.attestation_value_dialog, null)
+            val attrInput = view.findViewById<TextInputEditText>(R.id.value_input)
+            when (idFormat) {
+                ID_METADATA_RANGE_18PLUS -> attrInput.inputType = InputType.TYPE_CLASS_NUMBER
+                ID_METADATA_RANGE_UNDERAGE -> attrInput.inputType = InputType.TYPE_CLASS_NUMBER
+                else -> attrInput.inputType = InputType.TYPE_TEXT_FLAG_AUTO_COMPLETE
+            }
+
+            builder.setView(view)
+                .setPositiveButton(
+                    R.string.fire,
+                    null
+                )
+                .setNegativeButton(
+                    R.string.cancel,
+                    DialogInterface.OnClickListener { _, _ ->
+                        setResult("", true)
+                    }
+                )
+                .setTitle("Attestation Requested")
+                .setMessage(Html.fromHtml("An attestation has been requested for <b>$attributeName</b> with format <b>$idFormat</b>."))
+            // Create the AlertDialog object and return it
+            val dialog = builder.create()
+            dialog.setOnShowListener {
+                val posBtn = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                posBtn.setOnClickListener {
+                    val inputValue = attrInput.text.toString()
+                    if (inputValue != "") {
+                        setResult(inputValue, false)
+                        dialog.dismiss()
+                    } else {
+                        attrInput.error = "Enter a value."
+                    }
+                }
+            }
+            dialog
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
---
+++ ig-ssi/src/main/java/nl/tudelft/trustchain/ssi/dialogs/PresentAttestationDialog.kt
@@ -0,0 +1,64 @@
+package nl.tudelft.trustchain.ssi.dialogs
+
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.text.Html
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
+import androidx.fragment.app.DialogFragment
+import nl.tudelft.trustchain.ssi.R
+
+class PresentAttestationDialog(
+    private val attributeName: String,
+    private val attributeValue: String
+) :
+    DialogFragment() {
+
+    private var mView: View? = null
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            val inflater = requireActivity().layoutInflater
+
+            @SuppressLint("InflateParams")
+            val view = inflater.inflate(R.layout.present_attestation_dialog, null)!!
+            mView = view
+            builder.setView(mView)
+            builder.setTitle("hello")
+
+            val dialog: Dialog
+            val title = "Attestation for <font color='#EE0000'>${attributeName.capitalize()}</font>"
+            val message = "<b>$attributeName:</b> $attributeValue"
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                builder.setTitle(Html.fromHtml(title, Html.FROM_HTML_MODE_LEGACY))
+                dialog = builder.create()
+                dialog.setMessage(
+                    Html.fromHtml(message, Html.FROM_HTML_MODE_LEGACY)
+                )
+            } else {
+                @Suppress("DEPRECATION")
+                builder.setTitle(Html.fromHtml(title))
+                dialog = builder.create()
+                @Suppress("DEPRECATION")
+                dialog.setMessage(
+                    Html.fromHtml(message)
+                )
+            }
+            dialog
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    fun setQRCode(bitmap: Bitmap) {
+        val progressBar = mView!!.findViewById<ProgressBar>(R.id.progressBar)
+        if (progressBar.isVisible) {
+            progressBar.visibility = View.GONE
+        }
+        mView!!.findViewById<ImageView>(R.id.qrCodeView).setImageBitmap(bitmap)
+    }
+}
