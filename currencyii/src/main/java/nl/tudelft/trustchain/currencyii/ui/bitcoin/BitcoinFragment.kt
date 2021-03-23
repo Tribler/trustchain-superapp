4cd9b19e0370b7bbf5638499be13a9a5800e2023
Steven <s.f.lambregts@student.tudelft.nl>
2021-03-23 16:55:54 +0100

    Add estimated balance line

--- currencyii/src/main/java/nl/tudelft/trustchain/currencyii/ui/bitcoin/BitcoinFragment.kt
+++ currencyii/src/main/java/nl/tudelft/trustchain/currencyii/ui/bitcoin/BitcoinFragment.kt
@@ -2,8 +2,8 @@ package nl.tudelft.trustchain.currencyii.ui.bitcoin
 
 import android.content.ClipData
 import android.content.ClipboardManager
-import android.content.Context
 import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
@@ -25,7 +25,7 @@ import nl.tudelft.trustchain.currencyii.coin.WalletManagerConfiguration
 import nl.tudelft.trustchain.currencyii.ui.BaseFragment
 import org.bitcoinj.core.Coin
 import org.bitcoinj.core.NetworkParameters
-import kotlin.concurrent.thread
+import org.bitcoinj.wallet.Wallet
 
 /**
  * A simple [Fragment] subclass.
@@ -109,11 +109,14 @@ class BitcoinFragment : BaseFragment(R.layout.fragment_bitcoin),
         }
 
         // If the user has too little bitcoin, he can press the button to get more,
-        // the amount is hardcoded on the server somewhere.
-        if (walletManager.kit.wallet().balance.isGreaterThan(Coin.parseCoin("5"))) {
+        // the amount that is added is hardcoded on the server somewhere.
+        @Suppress("DEPRECATION")
+        if (walletManager.kit.wallet().getBalance(Wallet.BalanceType.ESTIMATED).isGreaterThan(Coin.parseCoin("10"))) {
             add_btc.isClickable = false
-            add_btc.setBackgroundColor(Color.GRAY)
-            add_btc.setPadding(33, 27, 33, 27)
+            add_btc.background.setColorFilter(Color.GRAY, PorterDuff.Mode.MULTIPLY)
+            add_btc.setOnClickListener {
+                Toast.makeText(this.requireContext(), "You already have enough bitcoin don't you think?", Toast.LENGTH_SHORT).show()
+            }
         } else {
             add_btc.setOnClickListener {
                 addBTC(walletManager.protocolAddress().toString())
@@ -140,16 +143,21 @@ class BitcoinFragment : BaseFragment(R.layout.fragment_bitcoin),
 
         val walletManager = WalletManagerAndroid.getInstance()
 
-        walletBalance.text =
-            walletManager.kit.wallet().balance.toFriendlyString()
-        if (walletManager.params.id === NetworkParameters.ID_MAINNET) {
-            chosenNetwork.text = "Production Network"
-        } else if (walletManager.params.id === NetworkParameters.ID_REGTEST) {
-            chosenNetwork.text = "RegTest Network"
-        } else if (walletManager.params.id === NetworkParameters.ID_TESTNET) {
-            chosenNetwork.text = "TestNet Network"
-        } else {
-            chosenNetwork.text = "Unknown Network selected"
+        walletBalance.text = walletManager.kit.wallet().balance.toFriendlyString()
+        walletEstimatedBalance.text = walletManager.kit.wallet().getBalance(Wallet.BalanceType.ESTIMATED).toFriendlyString()
+        when {
+            walletManager.params.id === NetworkParameters.ID_MAINNET -> {
+                chosenNetwork.text = "Production Network"
+            }
+            walletManager.params.id === NetworkParameters.ID_REGTEST -> {
+                chosenNetwork.text = "RegTest Network"
+            }
+            walletManager.params.id === NetworkParameters.ID_TESTNET -> {
+                chosenNetwork.text = "TestNet Network"
+            }
+            else -> {
+                chosenNetwork.text = "Unknown Network selected"
+            }
         }
         val seed = walletManager.toSeed()
         walletSeed.text = "${seed.seed}, ${seed.creationTime}"
@@ -161,12 +169,11 @@ class BitcoinFragment : BaseFragment(R.layout.fragment_bitcoin),
 
     /**
      * Add bitcoin to the wallet
-     * @param context
      * @param address - The address where I have to send the BTC to.
      */
     private fun addBTC(address: String) {
         val queue = Volley.newRequestQueue(context)
-        val url = "http://131.180.27.224:8000?address=${address}"
+        val url = "http://131.180.27.224:8000?address=$address"
 
         val stringRequest = StringRequest(
             Request.Method.GET, url,
@@ -211,7 +218,11 @@ class BitcoinFragment : BaseFragment(R.layout.fragment_bitcoin),
                     2 -> BitcoinNetworkOptions.TEST_NET
                     3 -> BitcoinNetworkOptions.REG_TEST
                     else -> {
-                        Toast.makeText(this.requireContext(), "Please select a bitcoin network first", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            this.requireContext(),
+                            "Please select a bitcoin network first",
+                            Toast.LENGTH_SHORT
+                        ).show()
                         return
                     }
                 },
@@ -246,7 +257,7 @@ class BitcoinFragment : BaseFragment(R.layout.fragment_bitcoin),
         }, 1500)
     }
 
-    fun copyToClipboard(text: String) {
+    private fun copyToClipboard(text: String) {
         val clipboard = getSystemService(this.requireContext(), ClipboardManager::class.java)!!
         val clip = ClipData.newPlainText(text, text)
         clipboard.setPrimaryClip(clip)
