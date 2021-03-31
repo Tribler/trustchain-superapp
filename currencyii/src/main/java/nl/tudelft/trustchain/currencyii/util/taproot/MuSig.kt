ea22032ac32a9cfa71d5c352f50fa744d5ef4c23
Rick Huisman <rick.huisman@planet.nl>
2021-03-31 15:59:27 +0200

    implement aggregate_schnorr_nonces

--- currencyii/src/main/java/nl/tudelft/trustchain/currencyii/util/taproot/MuSig.kt
+++ currencyii/src/main/java/nl/tudelft/trustchain/currencyii/util/taproot/MuSig.kt
@@ -1,6 +1,7 @@
 package nl.tudelft.trustchain.currencyii.util.taproot
 
 import nl.tudelft.ipv8.util.sha256
+import nl.tudelft.ipv8.util.toHex
 import org.bitcoinj.core.ECKey
 import org.bouncycastle.math.ec.ECPoint
 import java.math.BigInteger
@@ -8,7 +9,30 @@ import java.math.BigInteger
 class MuSig {
 
     companion object {
-        public fun generate_musig_key(pubKeys: List<ECKey>): Pair<HashMap<ECKey, ByteArray>, ECPoint> {
+        fun aggregate_schnorr_nonces(nonceList: List<ECKey>): Pair<ECPoint, Boolean> {
+            var r_agg: ECPoint? = null
+
+            for (key in nonceList) {
+                r_agg = if (r_agg == null) {
+                    key.pubKeyPoint
+                } else {
+                    r_agg.add(key.pubKeyPoint)
+                }
+            }
+
+            val r_agg_affine = Triple(r_agg!!.xCoord, r_agg.yCoord, r_agg.zCoords)
+            var negated = false
+
+            // doesn't seem to work correctly yet :(
+            if (Schnorr.jacobi(r_agg_affine.second.toBigInteger()) != BigInteger.ONE) {
+                negated = true
+                r_agg = r_agg.negate()
+            }
+
+            return Pair(r_agg!!, negated)
+        }
+
+        fun generate_musig_key(pubKeys: List<ECKey>): Pair<HashMap<ECKey, ByteArray>, ECPoint> {
             val pubKeyList = ArrayList<BigInteger>()
 
             for (key in pubKeys) {
--- currencyii/src/test/java/nl/tudelft/trustchain/currencyii/util/taproot/MuSigTest.kt
+++ currencyii/src/test/java/nl/tudelft/trustchain/currencyii/util/taproot/MuSigTest.kt
@@ -11,12 +11,22 @@ class MuSigTest {
 
     @Test
     fun generate_musig_key() {
-
         val key1 = ECKey.fromPrivate(BigInteger("88218786999700320424912157840922001183470238663577897435520060565802125439712"))
         val key2 = ECKey.fromPrivate(BigInteger("11756621930195768229168784074199362003209438395325908648574429387730312779458"))
 
         val expected = "023dd5fc3c1766d0a73466a5997da83efcc529107c9ecd0c56e2a28519f0eb3104"
         val actual = MuSig.generate_musig_key(listOf(key1, key2)).second.getEncoded(true).toHex()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun aggregate_schnorr_nonces() {
+        val key1 = ECKey.fromPrivate(BigInteger("2c68916c316d82ea8f3ebb27037354741ce080464590268780ffca750c5727f6", 16))
+        val key2 = ECKey.fromPrivate(BigInteger("08d538794fcc7766ecc1e0fd9c1642e5cd5147c67807cce4d46144db5fcc8534", 16))
+
+        val expected = "03326be2da46e2af92df7df4affacc9afdd8b0fb80e7958da65da62b6fc67883f4"
+        val actual = MuSig.aggregate_schnorr_nonces(listOf(key1, key2)).first.getEncoded(true).toHex()
 
         assertEquals(expected, actual)
     }
