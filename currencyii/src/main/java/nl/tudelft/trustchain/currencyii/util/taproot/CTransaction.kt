7ea96aa2a51ad368c54e072ec933db16b162126e
Rick Huisman <rick.huisman@planet.nl>
2021-04-19 11:52:18 +0200

    Fix unsigned Int serialization bug

--- currencyii/src/main/java/nl/tudelft/trustchain/currencyii/util/taproot/CTransaction.kt
+++ currencyii/src/main/java/nl/tudelft/trustchain/currencyii/util/taproot/CTransaction.kt
@@ -127,7 +127,7 @@ class CTransaction(
 
                 temp = byteArrayOf()
                 for (i in txTo.vin) {
-                    temp += littleEndian(i.nSequence.toUInt())
+                    temp += littleEndian(i.nSequence)
                 }
                 ssBuf += sha256(temp)
             }
@@ -239,7 +239,7 @@ class CTxIn(
         var r: ByteArray = byteArrayOf()
         r += prevout.serialize()
         r += Messages.serString(scriptSig)
-        r += littleEndian(nSequence.toUInt())
+        r += littleEndian(nSequence)
         return r
     }
 
@@ -252,6 +252,12 @@ class CTxIn(
         return this
     }
 }
+
+fun ByteArray.getUIntAt(idx: Int) =
+    ((this[idx].toUInt() and 0xFFu) shl 24) or
+        ((this[idx + 1].toUInt() and 0xFFu) shl 16) or
+        ((this[idx + 2].toUInt() and 0xFFu) shl 8) or
+        (this[idx + 3].toUInt() and 0xFFu)
 
 class CTxOut(
     var nValue: Long = 0,
@@ -314,7 +320,7 @@ class CTxWitness(
 fun littleEndian(uint: UInt): ByteArray {
     val bb: ByteBuffer = ByteBuffer.allocate(4)
     bb.order(ByteOrder.LITTLE_ENDIAN)
-    bb.put(uint.toByte())
+    bb.putInt(uint.toInt())
     return bb.array()
 }
 
