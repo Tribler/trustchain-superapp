2691560694b3d18885e5fa22465fe8ec07cfa0ac
Rick Huisman <rick.huisman@planet.nl>
2021-04-20 20:21:57 +0200

    Refresh join block after transfer funds

--- currencyii/src/main/java/nl/tudelft/trustchain/currencyii/ui/bitcoin/SharedWalletTransaction.kt
+++ currencyii/src/main/java/nl/tudelft/trustchain/currencyii/ui/bitcoin/SharedWalletTransaction.kt
@@ -98,7 +98,7 @@ class SharedWalletTransaction : BaseFragment(R.layout.fragment_shared_wallet_tra
         val swJoinBlock: TrustChainBlock =
             getCoinCommunity().fetchLatestSharedWalletBlock(blockHash!!)
                 ?: throw IllegalStateException("Shared Wallet not found given the hash: ${blockHash!!}")
-//        val walletData = SWJoinBlockTransactionData(swJoinBlock.transaction).getData()
+        val walletData = SWJoinBlockTransactionData(swJoinBlock.transaction).getData()
 
         if (getBalance().minus(Coin.valueOf(satoshiTransferAmount)).isNegative) {
             activity?.runOnUiThread {
@@ -128,6 +128,7 @@ class SharedWalletTransaction : BaseFragment(R.layout.fragment_shared_wallet_tra
         val responses = collectResponses(transferFundsData)
         try {
             getCoinCommunity().transferFunds(
+                walletData,
                 swJoinBlock.transaction,
                 transferFundsData.getData(),
                 responses,
