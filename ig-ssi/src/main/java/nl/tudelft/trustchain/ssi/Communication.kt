19e74b16699bc30f3e217a12f9fa5e0be7762188
Rowdy Chotkan <rowdy_chotkan@msn.com>
2021-04-25 19:45:01 +0200

    Add communication singleton class

---
+++ ig-ssi/src/main/java/nl/tudelft/trustchain/ssi/Communication.kt
@@ -0,0 +1,62 @@
+package nl.tudelft.trustchain.ssi
+
+import nl.tudelft.ipv8.attestation.CommunicationChannel
+import nl.tudelft.ipv8.attestation.CommunicationManager
+
+const val DEFAULT_PSEUDONYM = "MY_PEER"
+val DEFAULT_RENDEZVOUS_TOKEN = null
+
+object Communication {
+
+    private var communicationManager: CommunicationManager? = null
+    private var activePseudonym: String? = null
+    private var activeRendezvousToken: String? = null
+
+    val pseudonymLock = Object()
+    val tokenLock = Object()
+
+    fun getInstance(): CommunicationManager {
+        return communicationManager
+            ?: throw IllegalStateException("CommunicationManager is not initialized.")
+    }
+
+    fun load(pseudonym: String = getActivePseudonym(), rendezvous: String? = getActiveRendezvousToken()): CommunicationChannel {
+        var rendezvousToken = rendezvous
+        if (rendezvousToken == "") {
+            rendezvousToken = DEFAULT_RENDEZVOUS_TOKEN
+        }
+        if (rendezvousToken != activeRendezvousToken) {
+            setActiveRendezvousToken(rendezvousToken)
+        }
+        return getInstance().load(pseudonym, rendezvousToken)
+    }
+
+    fun getActivePseudonym(): String {
+        return activePseudonym
+            ?: throw java.lang.IllegalStateException("ActivePseudonym is not initialized.")
+    }
+
+    fun getActiveRendezvousToken(): String? {
+        return activeRendezvousToken
+    }
+
+    fun setActivePseudonym(pseudonym: String) {
+        synchronized(pseudonymLock) {
+            this.activePseudonym = pseudonym
+        }
+    }
+
+    fun setActiveRendezvousToken(token: String?) {
+        synchronized(pseudonymLock) {
+            this.activeRendezvousToken = token
+        }
+    }
+
+    class Factory(communicationManager: CommunicationManager) {
+        init {
+            Communication.communicationManager = communicationManager
+            activePseudonym = DEFAULT_PSEUDONYM
+            communicationManager.load(getActivePseudonym())
+        }
+    }
+}
