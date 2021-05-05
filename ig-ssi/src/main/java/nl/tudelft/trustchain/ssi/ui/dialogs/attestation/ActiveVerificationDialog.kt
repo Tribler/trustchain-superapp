7203f4a1c144039eee36adadd3b58f82a84bba8f
Rowdy Chotkan <rowdy_chotkan@msn.com>
2021-05-05 14:57:50 +0200

    Add active verification of attestations

---
+++ ig-ssi/src/main/java/nl/tudelft/trustchain/ssi/ui/dialogs/attestation/ActiveVerificationDialog.kt
@@ -0,0 +1,90 @@
+package nl.tudelft.trustchain.ssi.ui.dialogs.attestation
+
+import SuccessDialog
+import android.app.Dialog
+import android.content.Context
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import nl.tudelft.ipv8.Peer
+import nl.tudelft.trustchain.ssi.R
+import nl.tudelft.trustchain.ssi.ui.dialogs.status.DangerDialog
+
+class ActiveVerificationDialog :
+    DialogFragment() {
+    private lateinit var mView: View
+    private lateinit var mContext: Context
+    private lateinit var peer: Peer
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            // Use the Builder class for convenient dialog construction
+            val builder = AlertDialog.Builder(it)
+            val inflater = requireActivity().layoutInflater
+
+            val view = inflater.inflate(R.layout.active_verification_dialog, null)
+            this.mView = view
+            this.mContext = requireContext()
+
+            builder.setView(view)
+                .setNegativeButton(
+                    R.string.cancel
+                ) { _, _ -> }
+                .setTitle("Locating Client")
+            // Create the AlertDialog object and return it
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    fun challengePeer(peer: Peer) {
+        Handler(Looper.getMainLooper()).post {
+            this.peer = peer
+            mView.findViewById<TextView>(R.id.loadingInformation).text =
+                getString(R.string.challenging_client)
+            val progressBar = mView.findViewById<ProgressBar>(R.id.progressBar)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                progressBar.indeterminateDrawable.colorFilter =
+                    BlendModeColorFilter(Color.BLUE, BlendMode.SRC_IN)
+            } else {
+                @Suppress("DEPRECATION")
+                progressBar.indeterminateDrawable
+                    .setColorFilter(Color.BLUE, PorterDuff.Mode.SRC_IN)
+            }
+
+            this.dialog!!.setTitle("Sending Challenges")
+        }
+    }
+
+    fun setResult(result: Boolean) {
+        Handler(Looper.getMainLooper()).post {
+            this.dismiss()
+            if (result) {
+                SuccessDialog().show(
+                    parentFragmentManager,
+                    "ig-ssi"
+                )
+            } else {
+                DangerDialog().show(parentFragmentManager, "ig-ssi")
+            }
+        }
+    }
+
+    fun cancel(cancellationMessage: String) {
+        Handler(Looper.getMainLooper()).post {
+            Toast.makeText(requireContext(), cancellationMessage, Toast.LENGTH_LONG).show()
+            this.dismiss()
+        }
+    }
+}
--- ig-ssi/src/main/java/nl/tudelft/trustchain/ssi/ui/dialogs/attestation/AttestationConfirmationDialog.kt
+++ ig-ssi/src/main/java/nl/tudelft/trustchain/ssi/ui/dialogs/attestation/AttestationConfirmationDialog.kt
@@ -5,20 +5,32 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+import nl.tudelft.ipv8.Peer
 import nl.tudelft.ipv8.attestation.identity.Metadata
 import nl.tudelft.ipv8.keyvault.PublicKey
+import nl.tudelft.ipv8.util.stripSHA1Padding
 import nl.tudelft.ipv8.util.toHex
+import nl.tudelft.ipv8.util.toKey
 import nl.tudelft.trustchain.ssi.Communication
 import nl.tudelft.trustchain.ssi.ui.dialogs.status.DangerDialog
+
+const val VERIFICATION_THRESHOLD = 0.9
 
 class AttestationConfirmationDialog(
     private val attestationHash: ByteArray,
     private val attestationName: String,
     private val attestationValue: ByteArray?,
+    private val idFormat: String,
     private val metadata: Metadata,
     private val subjectKey: PublicKey,
     private val challengePair: Pair<ByteArray, Long>,
-    private val attestors: List<Pair<ByteArray, ByteArray>>
+    private val attestors: List<Pair<ByteArray, ByteArray>>,
+    private val rendezvousToken: String
 ) : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -48,9 +60,49 @@ class AttestationConfirmationDialog(
                     }
                 }
                 .setNegativeButton(
-                    "dismiss"
+                    "Active Verification"
                 ) { _, _ ->
-                    DangerDialog().show(parentFragmentManager, this.tag)
+                    val dialog = ActiveVerificationDialog()
+                    dialog.show(parentFragmentManager, "ig-ssi")
+                    GlobalScope.launch {
+                        val channel =
+                            Communication.load(rendezvous = rendezvousToken)
+                        var peer: Peer? = null
+                        try {
+                            withTimeout(30_000) {
+                                while (peer == null) {
+                                    peer =
+                                        channel.peers.find { it1 ->
+                                            it1.publicKey.keyToHash()
+                                                .contentEquals(subjectKey.keyToHash())
+                                        }
+                                    delay(100)
+                                }
+                            }
+                            dialog.challengePeer(peer!!)
+                        } catch (e: TimeoutCancellationException) {
+                            dialog.cancel("Failed to locate peer.")
+                        }
+
+                        var success: Boolean? = null
+                        try {
+                            withTimeout(60_000) {
+                                // TODO: Add dialog for attestation value.
+                                val hash = stripSHA1Padding(attestationHash)
+                                channel.verify(peer!!, hash, listOf(attestationValue!!), idFormat)
+                                while (success == null) {
+                                    if (channel.verificationOutput[hash.toKey()]!!.all { output -> output.second != null }) {
+                                        success =
+                                            channel.verificationOutput[hash.toKey()]!!.all { output -> output.second!! > VERIFICATION_THRESHOLD }
+                                    }
+                                    delay(100)
+                                }
+                            }
+                            dialog.setResult(success!!)
+                        } catch (e: TimeoutCancellationException) {
+                            dialog.cancel("Peer failed to respond to verification request in time.")
+                        }
+                    }
                 }
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
