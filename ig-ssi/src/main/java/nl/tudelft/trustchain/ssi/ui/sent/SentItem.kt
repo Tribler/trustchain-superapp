c8c331865a534abd193feca7a7ec25c2246d1731
Rowdy Chotkan <rowdy_chotkan@msn.com>
2021-05-13 11:44:10 +0200

    Add sentitem equals

--- ig-ssi/src/main/java/nl/tudelft/trustchain/ssi/ui/sent/SentItem.kt
+++ ig-ssi/src/main/java/nl/tudelft/trustchain/ssi/ui/sent/SentItem.kt
@@ -1,7 +1,6 @@
 package nl.tudelft.trustchain.ssi.ui.sent
 
 import com.mattskala.itemadapter.Item
-import nl.tudelft.ipv8.attestation.communication.AttestationPresentation
 import nl.tudelft.ipv8.attestation.communication.SubjectAttestationPresentation
 
 class SentItem(val index: Int, val attestation: SubjectAttestationPresentation) : Item() {
@@ -11,10 +10,16 @@ class SentItem(val index: Int, val attestation: SubjectAttestationPresentation) 
     }
 
     override fun areItemsTheSame(other: Item): Boolean {
-        return equals(other)
+        return this == other
     }
 
     override fun areContentsTheSame(other: Item): Boolean {
-        return areItemsTheSame(other)
+        return this == other
+    }
+
+    override fun hashCode(): Int {
+        var result = index
+        result = 31 * result + attestation.hashCode()
+        return result
     }
 }
