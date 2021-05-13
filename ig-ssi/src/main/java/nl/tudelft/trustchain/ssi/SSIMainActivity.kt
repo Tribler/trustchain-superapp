38adc9c74ae35dcfa0825361b9023c234ba52a7f
Rowdy Chotkan <rowdy_chotkan@msn.com>
2021-05-13 13:33:51 +0200

    Add revocation update callback to main activity

--- ig-ssi/src/main/java/nl/tudelft/trustchain/ssi/SSIMainActivity.kt
+++ ig-ssi/src/main/java/nl/tudelft/trustchain/ssi/SSIMainActivity.kt
@@ -5,22 +5,21 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.widget.*
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import nl.tudelft.ipv8.Peer
+import nl.tudelft.ipv8.util.toHex
 import nl.tudelft.trustchain.common.BaseActivity
 
 class SSIMainActivity : BaseActivity() {
     override val navigationGraph = R.navigation.nav_graph_ssi
     override val bottomNavigationMenu = R.menu.bottom_navigation_menu2
 
-    private lateinit var verificationFragment: Fragment
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val channel = Communication.load()
+        channel.revocationOverlay.setRevocationUpdateCallback(this::revocationUpdateCallback)
 
         // TODO: add callbacks for user update.
         // community.setAttestationRequestCallback(::attestationRequestCallback)
@@ -28,7 +27,7 @@ class SSIMainActivity : BaseActivity() {
         // community.setAttestationChunkCallback(::attestationChunkCallback)
 
         // Register own key as trusted authority.
-        channel.attestationOverlay.authorityManager.addTrustedAuthority(channel.myPeer.publicKey)
+        channel.authorityManager.addTrustedAuthority(channel.myPeer.publicKey)
         this.notificationHandler()
     }
 
@@ -57,6 +56,17 @@ class SSIMainActivity : BaseActivity() {
                 applicationContext,
                 "Received attestation chunk $i from ${peer.mid}.",
                 Toast.LENGTH_SHORT
+            )
+                .show()
+        }
+    }
+
+    private fun revocationUpdateCallback(publicKeyHash: ByteArray, version: Long, amount: Int) {
+        Handler(Looper.getMainLooper()).post {
+            Toast.makeText(
+                applicationContext,
+                "Received $amount revocation(s) from ${publicKeyHash.toHex()}, version $version.",
+                Toast.LENGTH_LONG
             )
                 .show()
         }
