9a577426f49a8d2dbd9e208fb41e6b462c7108d7
Rowdy Chotkan <rowdy_chotkan@msn.com>
2021-05-13 13:20:10 +0200

    Add sign date to SentItem

--- ig-ssi/src/main/java/nl/tudelft/trustchain/ssi/ui/sent/SentItemRenderer.kt
+++ ig-ssi/src/main/java/nl/tudelft/trustchain/ssi/ui/sent/SentItemRenderer.kt
@@ -1,11 +1,15 @@
 package nl.tudelft.trustchain.ssi.ui.sent
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.view.View
 import com.mattskala.itemadapter.ItemLayoutRenderer
 import kotlinx.android.synthetic.main.item_sent_attestation.view.*
 import nl.tudelft.ipv8.util.toHex
 import nl.tudelft.trustchain.ssi.R
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class SentItemRenderer(
     private val onRevoke: (SentItem) -> Unit,
@@ -19,10 +23,19 @@ class SentItemRenderer(
         val it = item.attestation
         publicKey.text = it.publicKey.keyToHash().toHex()
         attributeName.text = it.attributeName
+        val sdf = SimpleDateFormat("dd/MM/yy hh:mm a", Locale.getDefault())
+        val netDate = Date(it.signDate.toLong() * 1000)
+        signDate.text = sdf.format(netDate)
         metadata.text = String(it.metadata.serializedMetadata)
 
-        revokeButton.setOnClickListener {
-            onRevoke(item)
+        revokeButton.isActivated = !it.isRevoked
+        revokeButton.isClickable = !it.isRevoked
+        if (it.isRevoked) {
+            revokeButton.setTextColor(Color.GRAY);
+        } else {
+            revokeButton.setOnClickListener {
+                onRevoke(item)
+            }
         }
 
         setOnClickListener {
