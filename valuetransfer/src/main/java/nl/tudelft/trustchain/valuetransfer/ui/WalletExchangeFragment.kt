58ead10c14477d9eab0e0b185ab6f8b94fb05a59
Joost <j.w.bambacht@student.tudelft.nl>
2021-07-23 08:19:24 +0200

    remove unused files

--- valuetransfer/src/main/java/nl/tudelft/trustchain/valuetransfer/ui/WalletExchangeFragment.kt
+++
@@ -1,8 +0,0 @@
-package nl.tudelft.trustchain.valuetransfer.ui
-
-import nl.tudelft.trustchain.common.ui.BaseFragment
-import nl.tudelft.trustchain.valuetransfer.R
-
-class WalletExchangeFragment : BaseFragment(R.layout.fragment_wallet_exchange) {
-
-}
