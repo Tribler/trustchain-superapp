170cce79e4656331417c3abeb4807102ddffc2dc
Keon Chen <keon.chen@gmail.com>
2022-03-08 23:38:52 +0100

    [init] Init literatureDao app

---
+++ literaturedao/src/main/java/nl/tudelft/trustchain/literaturedao/LiteratureDaoActivity.kt
@@ -0,0 +1,8 @@
+package nl.tudelft.trustchain.literaturedao
+import nl.tudelft.trustchain.common.BaseActivity
+
+class LiteratureDaoActivity : BaseActivity() {
+    override val navigationGraph: Int = R.navigation.nav_literaturedao
+    override val bottomNavigationMenu: Int
+        get() = super.bottomNavigationMenu
+}
