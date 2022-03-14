c39ad415be1052d759feecaab8d8a47191ebb54a
Konrad Ponichtera <konpon96@gmail.com>
2022-03-15 00:21:38 +0100

    Trade offers fragment

---
+++ atomic-swap/src/main/java/nl/tudelft/trustchain/atomicswap/ui/tradeoffers/TradeOffersFragment.kt
@@ -0,0 +1,6 @@
+package nl.tudelft.trustchain.atomicswap.ui.tradeoffers
+
+import nl.tudelft.trustchain.atomicswap.R
+import nl.tudelft.trustchain.common.ui.BaseFragment
+
+class TradeOffersFragment : BaseFragment(R.layout.fragment_atomic_trade_offers)
