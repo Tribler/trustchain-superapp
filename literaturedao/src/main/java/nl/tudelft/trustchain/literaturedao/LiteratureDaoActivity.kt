427e609a21878522addc8541c45a0ad293565cef
Marko Matusovic <m.matusovic@student.tudelft.nl>
2022-03-16 19:46:42 +0100

    Attempt to debug opening file.

--- literaturedao/src/main/java/nl/tudelft/trustchain/literaturedao/LiteratureDaoActivity.kt
+++ literaturedao/src/main/java/nl/tudelft/trustchain/literaturedao/LiteratureDaoActivity.kt
@@ -1,8 +1,21 @@
 package nl.tudelft.trustchain.literaturedao
+import android.util.Log
 import nl.tudelft.trustchain.common.BaseActivity
+import nl.tudelft.trustchain.literaturedao.controllers.KeywordExtractor
 
 open class LiteratureDaoActivity : BaseActivity() {
     override val navigationGraph: Int = R.navigation.nav_literaturedao
     override val bottomNavigationMenu: Int
         get() = super.bottomNavigationMenu
+
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d("litdao", "starting ...")
+
+        KeywordExtractor().actualImplementation("hello please banana I have a person")
+    }
+
+
 }
