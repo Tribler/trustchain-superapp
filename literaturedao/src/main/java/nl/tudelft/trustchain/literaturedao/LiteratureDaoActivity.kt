136f2fe5845a78380491e7ac72dd69af6a9c8f6d
admin <siskocc@gmail.com>
2022-03-17 00:25:16 +0100

    End of the evening commit with working pdf ripper and keyword extractor

--- literaturedao/src/main/java/nl/tudelft/trustchain/literaturedao/LiteratureDaoActivity.kt
+++ literaturedao/src/main/java/nl/tudelft/trustchain/literaturedao/LiteratureDaoActivity.kt
@@ -1,20 +1,40 @@
 package nl.tudelft.trustchain.literaturedao
 import android.util.Log
+import com.tom_roush.pdfbox.android.PDFBoxResourceLoader
 import nl.tudelft.trustchain.common.BaseActivity
 import nl.tudelft.trustchain.literaturedao.controllers.KeywordExtractor
+import nl.tudelft.trustchain.literaturedao.controllers.PdfController
+import java.io.InputStream
 
 open class LiteratureDaoActivity : BaseActivity() {
     override val navigationGraph: Int = R.navigation.nav_literaturedao
     override val bottomNavigationMenu: Int
         get() = super.bottomNavigationMenu
 
-
     override fun onStart() {
         super.onStart()
+        Log.e("litdao", "starting ...")
+        PDFBoxResourceLoader.init(getApplicationContext());
+        var pdfController = PdfController()
+        var i = 1
+        while (i < 12){
+            val stream: InputStream = getAssets().open(i.toString() + ".pdf")
+            val result = KeywordExtractor()
+                .quikFix(pdfController
+                    .stripText(stream))
+                .toString()
 
-        Log.d("litdao", "starting ...")
+            Log.e("litdao", "litdao: " + result)
+            i += 1
+        }
+        //Log.d("litdao", pdfController.stripText(stream))
 
-        KeywordExtractor().actualImplementation("hello please banana I have a person")
+/*
+        Snackbar sb = Snackbar.make(findViewById(R.id.linearLayout), R.string.offline_message, Snackbar.LENGTH);
+        snackbar.show
+        ()
+*/
+        //Toast.makeText(LiteratureDaoActivity(), result, Toast.LENGTH_SHORT).show();
     }
 
 
