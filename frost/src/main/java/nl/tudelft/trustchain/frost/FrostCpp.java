9124b4c108df06190ab10c0a40297ac81a50f035
Andrei <andrei.al.stefan@gmail.com>
2022-03-16 18:35:07 +0100

    add bitcoinj-frost code into frost module; crashes on any function calls from NativeSecp256k1

---
+++ frost/src/main/java/nl/tudelft/trustchain/frost/FrostCpp.java
@@ -0,0 +1,14 @@
+package nl.tudelft.trustchain.frost;
+
+public class FrostCpp {
+
+//    static {
+//        System.loadLibrary("hello_cmake");
+//    }
+//    public native static String stringFromJNI();
+//
+//    static{
+//        System.loadLibrary("superappbitcoinjfrost");
+//    }
+//    public native static byte[][] generateKeyPair();
+}
