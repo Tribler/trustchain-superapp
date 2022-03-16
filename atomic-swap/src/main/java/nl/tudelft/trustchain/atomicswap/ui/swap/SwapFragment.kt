591508666d9d3f4bf8e1c6c213c939c6e685e8b1
Konrad Ponichtera <konpon96@gmail.com>
2022-03-16 22:17:49 +0100

    Swap fragment

--- atomic-swap/src/main/java/nl/tudelft/trustchain/atomicswap/ui/swap/SwapFragment.kt
+++ atomic-swap/src/main/java/nl/tudelft/trustchain/atomicswap/ui/swap/SwapFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.ViewModelProvider
 import nl.tudelft.trustchain.atomicswap.R
 import nl.tudelft.trustchain.atomicswap.databinding.FragmentAtomicSwapBinding
 import nl.tudelft.trustchain.common.ui.BaseFragment
+import java.math.BigDecimal
+
 
 class SwapFragment : BaseFragment(R.layout.fragment_atomic_swap) {
 
@@ -37,6 +41,15 @@ class SwapFragment : BaseFragment(R.layout.fragment_atomic_swap) {
         val fromCurrencySpinner = binding.fromCurrencySpinner
         val toCurrencySpinner = binding.toCurrencySpinner
 
+        val fromCurrencyInput = binding.fromCurrencyInput
+        val toCurrencyInput = binding.toCurrencyInput
+
+        val createSwapOfferButton = binding.createSwapOfferButton
+
+        model.createSwapOfferEnabled.observe(viewLifecycleOwner) {
+            createSwapOfferButton.isEnabled = it
+        }
+
         ArrayAdapter.createFromResource(
             requireContext(),
             R.array.currency_codes,
@@ -46,12 +59,40 @@ class SwapFragment : BaseFragment(R.layout.fragment_atomic_swap) {
             fromCurrencySpinner.adapter = adapter
             toCurrencySpinner.adapter = adapter
         }
+
+        fromCurrencyInput.addTextChangedListener { validateInput() }
+        toCurrencyInput.addTextChangedListener { validateInput() }
+
+        createSwapOfferButton.setOnClickListener { createSwapOffer() }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
         _model = null
+    }
+
+    private fun validateInput() {
+        val fromCurrencyAmount = binding.fromCurrencyInput.text.toString().toBigDecimalOrNull()
+        val toCurrencyAmount = binding.toCurrencyInput.text.toString().toBigDecimalOrNull()
+
+        model.setSwapOfferEnabled(
+            fromCurrencyAmount != null && fromCurrencyAmount > BigDecimal.ZERO
+                && toCurrencyAmount != null && toCurrencyAmount > BigDecimal.ZERO
+        )
+    }
+
+    private fun createSwapOffer() {
+        val fromCurrency = binding.fromCurrencySpinner.selectedItem
+        val toCurrency = binding.toCurrencySpinner.selectedItem
+
+        // Already validated before
+        val fromCurrencyAmount = binding.fromCurrencyInput.text.toString().toBigDecimal()
+        val toCurrencyAmount = binding.toCurrencyInput.text.toString().toBigDecimal()
+
+        // TODO Implement making swap offer
+        val input = "$fromCurrencyAmount $fromCurrency -> $toCurrencyAmount $toCurrency"
+        Toast.makeText(requireContext(), input, Toast.LENGTH_SHORT).show()
     }
 
 }
