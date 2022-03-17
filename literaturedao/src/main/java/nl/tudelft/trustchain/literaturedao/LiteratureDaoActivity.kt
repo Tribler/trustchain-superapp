74930c35b1cd0712496ee1b62275555cfa69e9e2
Marko Matusovic <m.matusovic@student.tudelft.nl>
2022-03-17 09:29:59 +0100

    Fix UI, add navigation

--- literaturedao/src/main/java/nl/tudelft/trustchain/literaturedao/LiteratureDaoActivity.kt
+++ literaturedao/src/main/java/nl/tudelft/trustchain/literaturedao/LiteratureDaoActivity.kt
@@ -1,21 +1,16 @@
 package nl.tudelft.trustchain.literaturedao
-import android.util.Log
 import nl.tudelft.trustchain.common.BaseActivity
-import nl.tudelft.trustchain.literaturedao.controllers.KeywordExtractor
 
 open class LiteratureDaoActivity : BaseActivity() {
-    override val navigationGraph: Int = R.navigation.nav_literaturedao
-    override val bottomNavigationMenu: Int
-        get() = super.bottomNavigationMenu
+    override val navigationGraph = R.navigation.nav_literaturedao
+    override val bottomNavigationMenu = R.menu.literature_navigation_menu
 
-
-    override fun onStart() {
-        super.onStart()
-
-        Log.d("litdao", "starting ...")
-
-        KeywordExtractor().actualImplementation("hello please banana I have a person")
-    }
-
+//    override fun onStart() {
+//        super.onStart()
+//
+//        Log.d("litdao", "starting ...")
+//
+//        KeywordExtractor().actualImplementation("hello please banana I have a person")
+//    }
 
 }
---
+++ literaturedao/src/main/java/nl/tudelft/trustchain/literaturedao/ui/LibraryFragment.kt
@@ -0,0 +1,16 @@
+package nl.tudelft.trustchain.literaturedao.ui
+
+import android.os.Bundle
+import nl.tudelft.trustchain.common.ui.BaseFragment
+import nl.tudelft.trustchain.literaturedao.R
+
+class LibraryFragment : BaseFragment(R.layout.fragment_library) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
