84df59cb430090af3382b0168b74a02bb34a999a
Marko Matusovic <m.matusovic@student.tudelft.nl>
2022-03-17 18:38:25 +0100

    Change the view of my literature

--- literaturedao/src/main/java/nl/tudelft/trustchain/literaturedao/ui/MyLiteratureFragment.kt
+++ literaturedao/src/main/java/nl/tudelft/trustchain/literaturedao/ui/MyLiteratureFragment.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import nl.tudelft.trustchain.common.ui.BaseFragment
 import nl.tudelft.trustchain.literaturedao.R
 
-class MyLiteratureFragment : BaseFragment(R.layout.fragment_literature_add) {
+class MyLiteratureFragment : BaseFragment(R.layout.fragment_literature_overview) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
