070e1305f2e79d413ff1c72266e635f3f0c5f463
Marko Matusovic <m.matusovic@student.tudelft.nl>
2022-03-17 09:29:59 +0100

    Fix UI, add navigation

---
+++ literaturedao/src/main/java/nl/tudelft/trustchain/literaturedao/ui/MyLiteratureFragment.kt
@@ -0,0 +1,16 @@
+package nl.tudelft.trustchain.literaturedao.ui
+
+import android.os.Bundle
+import nl.tudelft.trustchain.common.ui.BaseFragment
+import nl.tudelft.trustchain.literaturedao.R
+
+class MyLiteratureFragment : BaseFragment(R.layout.fragment_literature_add) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
