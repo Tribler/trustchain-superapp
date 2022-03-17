37947ca924eaf4a58a9b67b3796dc9b4daf4e5b3
Rohan Madhwal <rohan@madhwal.com>
2022-03-17 13:57:43 +0100

    Update AppGossiper.kt

--- freedomOfComputing/src/main/java/nl/tudelft/trustchain/FOC/AppGossiper.kt
+++ freedomOfComputing/src/main/java/nl/tudelft/trustchain/FOC/AppGossiper.kt
@@ -20,7 +20,7 @@ import java.util.concurrent.CountDownLatch
 import kotlin.collections.ArrayList
 
 /**
- * This gossips data about apps with a few random peers every couple seconds
+ * This gossips data about 10 random apps with a peers every 10 seconds and fetches new apps every 20 seconds
  */
 
 lateinit var appGossiperInstance: AppGossiper
