ad8eabe99e0911ce2d5ca2591c0bbcba17cd4ba2
Nieuwlaar <nieuwlaar@gmail.com>
2022-04-06 20:45:02 +0200

    added amount validation unit tests

--- valuetransfer/src/androidTest/java/nl/tudelft/trustchain/valuetransfer/dialogs/ExchangeTransferMoneyLinkDialogTest.kt
+++ valuetransfer/src/androidTest/java/nl/tudelft/trustchain/valuetransfer/dialogs/ExchangeTransferMoneyLinkDialogTest.kt
@@ -19,5 +19,27 @@ class ExchangeTransferMoneyLinkDialogTest{
         assertEquals(expected, true)
     }
 
+    @Test
+    fun `Too_large_transaction_amount`(){
+        val expected = exchange.isValidTransactionAmount("199999999999999.99")
+        assertEquals(expected, "Invalid")
+    }
 
+    @Test
+    fun `Random_transaction_amount`(){
+        val expected = exchange.isValidTransactionAmount("Hi")
+        assertEquals(expected, "Invalid")
+    }
+
+    @Test
+    fun `Valid_transaction_amount`(){
+        val expected = exchange.isValidTransactionAmount("10.00")
+        assertEquals(expected, "Valid")
+    }
+
+    @Test
+    fun `Valid_but_large_transaction_amount`(){
+        val expected = exchange.isValidTransactionAmount("2000.00")
+        assertEquals(expected, "Valid but large")
+    }
 }
