e549a18b5c0d0d9214980483809bb2cd29e7d351
rmadhwal <rohan@madhwal.com>
2022-04-14 18:43:19 +0200

    klint fixes

--- freedomOfComputing/src/main/java/nl/tudelft/trustchain/FOC/community/FOCCommunityBase.kt
+++ freedomOfComputing/src/main/java/nl/tudelft/trustchain/FOC/community/FOCCommunityBase.kt
@@ -7,7 +7,7 @@ import nl.tudelft.ipv8.messaging.eva.TransferProgress
 import java.util.*
 
 @Suppress("deprecation")
-abstract class FOCCommunityBase: Community() {
+abstract class FOCCommunityBase : Community() {
 
     abstract var torrentMessagesList: ArrayList<Pair<Peer, FOCMessage>>
 
