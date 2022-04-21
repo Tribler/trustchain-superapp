8f2e0817a594d4b13477fef48675c21de226f676
Keon Chen <keon.chen@gmail.com>
2022-04-22 00:26:35 +0200

    Use file path from external storage to generate torrent

--- literaturedao/src/main/java/nl/tudelft/trustchain/literaturedao/LiteratureDaoActivity.kt
+++ literaturedao/src/main/java/nl/tudelft/trustchain/literaturedao/LiteratureDaoActivity.kt
@@ -242,7 +242,10 @@ open class LiteratureDaoActivity : BaseActivity() {
                 outputStream.write(ins.readBytes())
                 ins.close()
                 outputStream.close()
-                this.createTorrent(DEFAULT_LITERATURE)
+                val torrent = this.createTorrent(DEFAULT_LITERATURE)
+                if (torrent != null) {
+                    literatureGossiper?.addTorrentInfo(torrent)
+                }
             }
         } catch (e: Exception) {
             this.printToast(e.toString())
@@ -253,8 +256,9 @@ open class LiteratureDaoActivity : BaseActivity() {
      * Creates a torrent from a file given as input
      * The extension of the file must be included (for example, .png)
      */
-    private fun createTorrent(fileName: String): TorrentInfo? {
-        val file = File(applicationContext.cacheDir.absolutePath + "/" + fileName.split("/").last())
+    fun createTorrent(filePath: String): TorrentInfo? {
+        val file = File(filePath)
+//        val file = File(applicationContext.cacheDir.absolutePath + "/" + fileName.split("/").last())
         if (!file.exists()) {
             runOnUiThread { printToast("Something went wrong, check logs") }
             Log.i("litdao", "File doesn't exist!")
@@ -278,7 +282,7 @@ open class LiteratureDaoActivity : BaseActivity() {
         val torrent = ct.generate()
         val buffer = torrent.bencode()
 
-        val torrentName = fileName.substringBeforeLast('.') + torrentDotExtension
+        val torrentName = filePath.substringBeforeLast('.') + torrentDotExtension
 
         var os: OutputStream? = null
         try {
@@ -297,7 +301,7 @@ open class LiteratureDaoActivity : BaseActivity() {
         val ti = TorrentInfo.bdecode(Vectors.byte_vector2bytes(buffer))
         val magnetLink = preHashString + ti.infoHash() + displayNameAppender + ti.name()
         Log.i("litdao", magnetLink)
-        runOnUiThread { printToast(fileName) }
+        runOnUiThread { printToast(filePath) }
         return ti
     }
 
@@ -349,8 +353,12 @@ open class LiteratureDaoActivity : BaseActivity() {
                     Log.d("litdao", "file name: " + d.name)
                     Log.d("litdao", "file path: " + d.uri.path)
                     Log.d("litdao", "file exists? " + d.exists().toString())
-                    copyFile(File(d.uri.path.toString().substringAfter(":")), File(applicationContext.cacheDir.absolutePath + "/" + d.name))
-                    createTorrent(d.name.toString())
+//                    copyFile(File(d.uri.path.toString().substringAfter(":")), File(applicationContext.cacheDir.absolutePath + "/" + d.name))
+//                    createTorrent(d.name.toString())
+                    val newTorrent = createTorrent(d.uri.path.toString())
+                    if (newTorrent != null) {
+                        literatureGossiper?.addTorrentInfo(newTorrent)
+                    }
                     var intent = Intent(Intent.ACTION_VIEW);
                     intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                     intent.setDataAndType(d.uri, "application/pdf");
