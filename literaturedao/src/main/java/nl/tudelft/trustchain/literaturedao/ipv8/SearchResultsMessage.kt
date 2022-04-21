eede81b513dd75eac5c3aee28029a28556f5ba6c
Marko Matusovic <m.matusovic@student.tudelft.nl>
2022-04-21 17:01:36 +0200

    Add back-end for remote search

---
+++ literaturedao/src/main/java/nl/tudelft/trustchain/literaturedao/ipv8/SearchResultsMessage.kt
@@ -0,0 +1,17 @@
+package nl.tudelft.trustchain.literaturedao.ipv8
+
+import com.google.gson.Gson
+import nl.tudelft.ipv8.messaging.Deserializable
+
+data class SearchResultsMessage(val results : SearchResultList) : nl.tudelft.ipv8.messaging.Serializable {
+    override fun serialize(): ByteArray {
+        return Gson().toJson(results).toByteArray()
+    }
+
+    companion object Deserializer : Deserializable<SearchResultsMessage> {
+        override fun deserialize(buffer: ByteArray, offset: Int): Pair<SearchResultsMessage, Int> {
+            var toReturn = Gson().fromJson(buffer.toString(Charsets.UTF_8), SearchResultList::class.java)
+            return Pair(SearchResultsMessage(toReturn), buffer.size)
+        }
+    }
+}
