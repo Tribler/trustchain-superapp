6269219225abc1753f047f16027733d836ac57fd
Keon Chen <keon.chen@gmail.com>
2022-05-10 09:26:56 +0200

    Fix cleanup

--- literaturedao/src/main/java/nl/tudelft/trustchain/literaturedao/MyLiteratureFragment.kt
+++ literaturedao/src/main/java/nl/tudelft/trustchain/literaturedao/MyLiteratureFragment.kt
@@ -12,61 +12,17 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import nl.tudelft.trustchain.literaturedao.data_types.Literature
 import nl.tudelft.trustchain.literaturedao.data_types.LocalData
-import java.io.BufferedReader
-import java.io.FileInputStream
-import java.io.FileNotFoundException
-import java.io.InputStreamReader
+import nl.tudelft.trustchain.literaturedao.utils.CacheUtil
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MyLiteratureFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MyLiteratureFragment : Fragment(R.layout.fragment_my_literature) {
 
     fun loadLocalData(): LocalData{
-        // Load local data
-        var fileInputStream: FileInputStream? = null
-
-
-        try {
-            try {
-                fileInputStream = context?.openFileInput("localData.json")
-            } catch (e: FileNotFoundException) {
-                context?.openFileOutput("localData.json", Context.MODE_PRIVATE).use { output ->
-                    output?.write(
-                        Json.encodeToString(LocalData(mutableListOf<Literature>())).toByteArray()
-                    )
-                }
-                fileInputStream = context?.openFileInput("localData.json")
-            }
-            var inputStreamReader: InputStreamReader = InputStreamReader(fileInputStream)
-            val bufferedReader: BufferedReader = BufferedReader(inputStreamReader)
-            val stringBuilder: StringBuilder = StringBuilder()
-            var text: String? = null
-            while ({ text = bufferedReader.readLine(); text }() != null) {
-                stringBuilder.append(text)
-            }
-            val localData: LocalData = Json.decodeFromString<LocalData>(stringBuilder.toString())
-            return localData
-        } catch (e: Exception) {
-            return LocalData(mutableListOf<Literature>());
-        }
+        return CacheUtil(context).loadLocalData()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("litDao", loadLocalData().toString())
     }
 
     override fun onCreateView(
