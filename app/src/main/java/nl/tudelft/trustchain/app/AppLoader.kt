e57f2ceb94437b1bbd29edc05037d9d9f8463f18
Rowdy Mitchell Chotkan <rowdy_chotkan@msn.com>
2023-02-20 15:42:22 +0100

    Add DeToks to default apps

--- app/src/main/java/nl/tudelft/trustchain/app/AppLoader.kt
+++ app/src/main/java/nl/tudelft/trustchain/app/AppLoader.kt
@@ -70,6 +70,7 @@ class AppLoader(
     companion object {
         val PREFERRED_APPS = stringSetPreferencesKey("preferred_apps")
         val DEFAULT_APPS = setOf(
+            AppDefinition.DETOKS.appName,
             AppDefinition.VALUETRANSFER.appName,
             AppDefinition.MUSIC_DAO.appName,
             AppDefinition.EIGHTEEN_PLUS.appName
