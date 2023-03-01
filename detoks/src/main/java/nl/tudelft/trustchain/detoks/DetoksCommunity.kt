a335cc0e4b1b1312f182b836a416cb46a87e30d3
nblagoev <n.blagoev@student.tudelft.nl>
2023-03-01 17:37:30 +0200

    Added community file

---
+++ detoks/src/main/java/nl/tudelft/trustchain/detoks/DetoksCommunity.kt
@@ -0,0 +1,69 @@
+package nl.tudelft.trustchain.detoks
+import nl.tudelft.ipv8.IPv4Address
+import nl.tudelft.ipv8.Community
+import nl.tudelft.ipv8.Peer
+import nl.tudelft.ipv8.android.IPv8Android
+import nl.tudelft.ipv8.attestation.trustchain.BlockSigner
+import nl.tudelft.ipv8.attestation.trustchain.TrustChainBlock
+import nl.tudelft.ipv8.messaging.payload.IntroductionResponsePayload
+import java.util.*
+
+
+class DetoksCommunity : Community() {
+    // TODO: WHAT THIS?
+    override val serviceId = "02313685c1912a141279f8248fc8db5899c5df5a"
+
+    val discoveredAddressesContacted: MutableMap<IPv4Address, Date> = mutableMapOf()
+    val lastTrackerResponses = mutableMapOf<IPv4Address, Date>()
+
+    override fun walkTo(address: IPv4Address) {
+        super.walkTo(address)
+
+        discoveredAddressesContacted[address] = Date()
+    }
+
+    // Retrieve the trustchain community
+
+    private fun getDetoksComm(): DetoksCommunity {
+        return IPv8Android.getInstance().getOverlay<DetoksCommunity>()
+            ?: throw IllegalStateException("TrustChainCommunity is not configured")
+    }
+
+    override fun onIntroductionResponse(peer: Peer, payload: IntroductionResponsePayload) {
+        super.onIntroductionResponse(peer, payload)
+
+        if (peer.address in DEFAULT_ADDRESSES) {
+            lastTrackerResponses[peer.address] = Date()
+        }
+    }
+
+//    fun registerTransactionValidator(blockType: Any, any: Any) {
+//
+//    }
+//
+//    fun registerBlockSigner(BLOCK_TYPE: Any?, param: BlockSigner) {
+//
+//    }
+//
+//    fun addListener(BLOCK_TYPE: Any?, param: Any) {
+//
+//    }
+//
+//    fun createAgreementBlock(block: TrustChainBlock, mapOf: Map<Any?, Any?>){
+//
+//    }
+
+}
+//class Blockchain(
+//    val id: UInt,
+//    val trustchain = IPV8Android.getInstance().getOverlay<TrustChainCommunity>()!!
+//) :
+//    Comparable<Blockchain>, Serializable {
+//    override fun compareTo(other: Blockchain): Int {
+//        return id.compareTo(other.id)
+//    }
+//
+//    override fun serialize(): ByteArray {
+//       return serializeUInt(id)
+//    }
+//}
