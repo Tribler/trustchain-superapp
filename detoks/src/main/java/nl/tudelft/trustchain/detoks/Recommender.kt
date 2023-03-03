930ce5b680fdcae8604dd93105b050c5bbd02844
Lucy Jense <twinkle@duck.com>
2023-03-03 16:35:44 +0100

    Added random selector for recommender

--- detoks/src/main/java/nl/tudelft/trustchain/detoks/Recommender.kt
+++ detoks/src/main/java/nl/tudelft/trustchain/detoks/Recommender.kt
@@ -1,4 +1,9 @@
 package nl.tudelft.trustchain.detoks
 
+import kotlin.random.Random
+
 class Recommender {
+    private fun coinTossRecommender(uris: Array<String>): String {
+        return uris[Random.nextInt(uris.size)]
+    }
 }
