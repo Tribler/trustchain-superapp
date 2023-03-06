617d59537e9760f3e94ed418c551c567009fc70a
AlbertoCastagnaro <alberto.castagnaro@gmail.com>
2023-03-06 01:11:21 +0100

    community init and token send to peers in the democommunity. Missing implementation of sending a token authenticated with ipv8 overlay.

--- detoks/src/main/java/nl/tudelft/trustchain/detoks/ExampleOverlayFragment.kt
+++ detoks/src/main/java/nl/tudelft/trustchain/detoks/ExampleOverlayFragment.kt
@@ -1,10 +1,13 @@
 package nl.tudelft.trustchain.detoks
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import nl.tudelft.ipv8.Community
+import nl.tudelft.ipv8.IPv8
 import nl.tudelft.ipv8.android.IPv8Android
 import nl.tudelft.ipv8.attestation.trustchain.TrustChainCommunity
+import nl.tudelft.ipv8.keyvault.PublicKey
+import nl.tudelft.trustchain.common.DemoCommunity
 import nl.tudelft.trustchain.common.ui.BaseFragment
 import nl.tudelft.trustchain.common.util.viewBinding
 import nl.tudelft.trustchain.detoks.databinding.FragmentExampleoverlayBinding
@@ -12,24 +15,34 @@ import nl.tudelft.trustchain.detoks.databinding.FragmentExampleoverlayBinding
 class ExampleOverlayFragment : BaseFragment(R.layout.fragment_exampleoverlay) {
 
     private val binding by viewBinding(FragmentExampleoverlayBinding::bind)
+    private lateinit var ipv8: IPv8
+    private lateinit var community: Community
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        val community = IPv8Android.getInstance().getOverlay<TrustChainCommunity>()!!
+        community = IPv8Android.getInstance().getOverlay<DemoCommunity>()!!
         val peers = community.getPeers()
 
-        for (peer in peers) {
-            Log.d("DemoApplication", peer.mid)
-        }
 
 
 //        // Example of how to change the page
 //        super.onViewCreated(view, savedInstanceState)
 //
-//        binding.button1.setOnClickListener {
-//            binding.textView1.setText("lmao")
-//        }
+        binding.button1.setOnClickListener {
+            for (peer in peers) {
+                sendToken(
+                    1,
+                    community.myPeer.publicKey,
+                    peer?.publicKey ?: community.myPeer.publicKey
+                )
+                binding.textView1.text = "Sent token"
+
+            }
+        }
 
     }
 
+    fun sendToken(value: Int, sender: PublicKey, receiver: PublicKey) {
+
+    }
 }
