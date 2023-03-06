f979c045e5e7d53a3ca72958c02ac275087be6f6
Chris Bras <chris.s.bras@gmail.com>
2023-03-06 11:41:11 +0100

    edited some stuff

--- util/communitysim.py
+++ util/communitysim.py
@@ -31,24 +31,13 @@ class MyCommunity(Community):
 
 
 async def start_communities():
-    for i in [2]:
+    for i in [1]:
         builder = ConfigBuilder().clear_keys().clear_overlays()
-        # If we actually want to communicate between two different peers
-        # we need to assign them different keys.
-        # We will generate an EC key called 'my peer' which has 'medium'
-        # security and will be stored in file 'ecI.pem' where 'I' is replaced
-        # by the peer number (1 or 2).
         builder.add_key("my peer", "medium", f"ec{i}.pem")
-        # Instruct IPv8 to load our custom overlay, registered in _COMMUNITIES.
-        # We use the 'my peer' key, which we registered before.
-        # We will attempt to find other peers in this overlay using the
-        # RandomWalk strategy, until we find 10 peers.
-        # We do not provide additional startup arguments or a function to run
-        # once the overlay has been initialized.
         builder.add_overlay(
             "MyCommunity",
             "my peer",
-            [WalkerDefinition(Strategy.RandomWalk, 3, {"timeout": 1.0})],
+            [WalkerDefinition(Strategy.RandomWalk, 20, {"timeout": 30.0})],
             default_bootstrap_defs,
             {},
             [("started",)],
