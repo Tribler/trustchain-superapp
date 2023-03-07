41f71388a9b1347c1a81bdd54bba70797e6c2da8
Jorge Romeu Huidobro <jorge.romeu.huidobro@gmail.com>
2023-03-07 11:54:26 +0100

    Created OurCommunity

--- detoks/src/main/java/nl/tudelft/trustchain/detoks/ExampleOverlayFragment.kt
+++ detoks/src/main/java/nl/tudelft/trustchain/detoks/ExampleOverlayFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import nl.tudelft.ipv8.Community
 import nl.tudelft.ipv8.IPv8
 import nl.tudelft.ipv8.android.IPv8Android
+import nl.tudelft.ipv8.attestation.trustchain.TrustChainCommunity
 import nl.tudelft.trustchain.common.DemoCommunity
 import nl.tudelft.trustchain.common.ui.BaseFragment
 import nl.tudelft.trustchain.common.util.viewBinding
@@ -18,11 +19,22 @@ class ExampleOverlayFragment : BaseFragment(R.layout.fragment_exampleoverlay) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        community = IPv8Android.getInstance().getOverlay<DemoCommunity>()!!
+        community = IPv8Android.getInstance().getOverlay<OurCommunity>()!!
 
+       // Example of how to change the page
+        binding.button1.setOnClickListener {
 
-//        // Example of how to change the page
-//        super.onViewCreated(view, savedInstanceState)
+            binding.textView1.text = community.serviceId;
+
+            binding.textView1.setText("peers:\n")
+
+            val peers = community.getPeers()
+
+            for (peer in peers) {
+                binding.textView1.append("${peer.address}, ${peer.key}\n")
+            }
+
+        }
 
 
     }
