214c79d4d2196803a34b1383a6289b09b30ddbd1
Lucy Jense <twinkle@duck.com>
2023-03-08 17:26:15 +0100

    Added a lot of TODOs to get a clear overview of what needs to be implemented

--- detoks/src/main/java/nl/tudelft/trustchain/detoks/Recommender.kt
+++ detoks/src/main/java/nl/tudelft/trustchain/detoks/Recommender.kt
@@ -2,9 +2,16 @@ package nl.tudelft.trustchain.detoks
 
 import kotlin.random.Random
 
+// TODO: Implement some kind of handler for all of this that also stores the user's own profile
+// TODO: On downloading a new torrent, check all the other profiles to rank it
+// TODO: On sending a video/peer discovery, attach the profile to keep peers updated
+// TODO: After watching a video, update the (new) hashmap entry
+// TODO: Generate 100 dummy profiles with dummy entries to do testing with
+// TODO: Compare the performance of coin toss against watch time
+
 class ProfileEntry(
-    val duration: Int,
-    val watchTime: Int,
+    val duration: Int,      // TODO: Change to time
+    val watchTime: Int,     // TODO: Change to time
     val freshness: Double,
 )
 
@@ -17,6 +24,7 @@ class Recommender {
         return URIs[Random.nextInt(URIs.size)]
     }
 
+    // TODO: Return a sorted list of videos not just the 'winner'
     private fun watchTimeRecommender(URIs: List<String>, profiles: List<Profile>): String {
         var (candidate, averagePercentageWatched) = Pair("", 0.0f)
         for(URI: String in URIs) {
