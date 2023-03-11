b0cdb2e869e80a877fe7ab309c6d4257a1a3a9e9
Ne184 <64256244+Ne184@users.noreply.github.com>
2023-03-11 10:20:52 +0100

    Added QRCode Scanner

---
+++ detoks/src/main/java/nl/tudelft/trustchain/detoks/OfflineTransferFragment.kt
@@ -0,0 +1,119 @@
+package nl.tudelft.trustchain.detoks
+
+import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import nl.tudelft.trustchain.common.util.QRCodeUtils
+import nl.tudelft.trustchain.common.ui.BaseFragment
+import org.json.JSONObject
+import nl.tudelft.ipv8.IPv8
+import java.security.PublicKey
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [OfflineTransferFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class OfflineTransferFragment : BaseFragment(R.layout.fragment_offline_transfer) {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private val qrCodeUtils by lazy {
+        QRCodeUtils(requireContext())
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_offline_transfer, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val myPublicKey = getIpv8().myPeer.publicKey.pub().keyToBin().toString()
+        val buttonRequest = view.findViewById<Button>(R.id.button_request)
+        buttonRequest.setOnClickListener {
+            showQR(view, myPublicKey)
+        }
+
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment OfflineTransferFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            OfflineTransferFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
+
+    }
+
+    private fun showQR(view: View, myPublicKey: String) {
+        val jsonObject = JSONObject()
+        jsonObject.put("public_key", myPublicKey)
+        val amountText = view.findViewById<EditText>(R.id.amount)
+        val amount = amountText.text
+        jsonObject.put("amount_requested", amount)
+        val jsonString = jsonObject.toString()
+        hideKeyboard()
+        lifecycleScope.launch {
+            val bitmap = withContext(Dispatchers.Default) {
+                qrCodeUtils.createQR(jsonString)
+            }
+            val qrCodeImage = view.findViewById<ImageView>(R.id.QR)
+            qrCodeImage.setImageBitmap(bitmap)
+        }
+        amountText.text.clear()
+    }
+
+    private fun hideKeyboard() {
+        val inputManager = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if (inputManager.isAcceptingText) {
+            inputManager.hideSoftInputFromWindow(activity?.currentFocus?.windowToken, 0)
+        }
+    }
+
+
+}
