48e79f74b70d3b34333b992bfe7720d060b6f4e5
Jorge Romeu Huidobro <jorge.romeu.huidobro@gmail.com>
2023-03-11 17:48:06 +0100

    Setup basic communication

--- detoks/src/main/java/nl/tudelft/trustchain/detoks/OurCommunity.kt
+++ detoks/src/main/java/nl/tudelft/trustchain/detoks/OurCommunity.kt
@@ -1,8 +1,28 @@
 package nl.tudelft.trustchain.detoks
 
+import android.util.Log
 import nl.tudelft.ipv8.Community
+import nl.tudelft.ipv8.messaging.Packet
 
 class OurCommunity : Community() {
     override val serviceId = "12313685c1912a191279f8248fc8db5899c5df6a"
+
+    private val MESSAGE_ID = 1
+
+    init {
+        messageHandlers[MESSAGE_ID] = ::onMessage
+    }
+
+    private fun onMessage(packet: Packet) {
+        val (peer, payload) = packet.getAuthPayload(Token.Deserializer)
+        Log.d("OurCommunity", peer.mid + ": " + payload.unique_id)
+    }
+
+    fun broadcastGreeting() {
+        for (peer in getPeers()) {
+            val packet = serializePacket(MESSAGE_ID, Token("Hello"))
+            send(peer.address, packet)
+        }
+    }
 }
 
--- detoks/src/main/java/nl/tudelft/trustchain/detoks/TestFragment.kt
+++ detoks/src/main/java/nl/tudelft/trustchain/detoks/TestFragment.kt
@@ -15,10 +15,11 @@ import nl.tudelft.ipv8.attestation.trustchain.TrustChainTransaction
 import nl.tudelft.trustchain.common.ui.BaseFragment
 import nl.tudelft.trustchain.common.util.viewBinding
 import nl.tudelft.trustchain.detoks.databinding.FragmentExampleoverlayBinding
+import nl.tudelft.trustchain.detoks.databinding.FragmentTestBinding
 
-class ExampleOverlayFragment : BaseFragment(R.layout.fragment_exampleoverlay) {
+class TestFragment : BaseFragment(R.layout.fragment_test) {
 
-    private val binding by viewBinding(FragmentExampleoverlayBinding::bind)
+    private val binding by viewBinding(FragmentTestBinding::bind)
 
     private lateinit var ipv8: IPv8
     private lateinit var community: OurCommunity
@@ -35,32 +36,11 @@ class ExampleOverlayFragment : BaseFragment(R.layout.fragment_exampleoverlay) {
         trustchainCommunity = IPv8Android.getInstance().getOverlay()!!
         ipv8 = IPv8Android.getInstance()
 
-
-        binding.peer1IpTextview.text = "${ipv8.myPeer.address}"
-
-
-
-
-//        // busy wait for peers
-//        var peers = community.getPeers()
-//        while (peers.isEmpty()) {
-//            peers = community.getPeers()
-//        }
-//
-//        binding.peer2IpTextview.text = "${peers[0].address}"
-
-
-        binding.peer1SendTokenButton.setOnClickListener {
-
-            Log.d("OurCommunity", "Broadcasting")
-
-            lifecycleScope.launch {
-                while (isActive) {
-                    community.broadcastGreeting()
-                    delay(1000)
-                }
-            }
+        binding.button1.setOnClickListener {
+            createProposal(ipv8.myPeer)
         }
+
+        binding.textView1.text = "asdf"
 
 
     }
