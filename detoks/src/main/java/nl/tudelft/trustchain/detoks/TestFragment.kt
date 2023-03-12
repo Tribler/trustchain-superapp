2292c1c52902571f155f809e926b5262542a9d01
AlbertoCastagnaro <alberto.castagnaro@gmail.com>
2023-03-12 19:30:15 +0100

    full basic transaction example without validation
    Transaction with local host:
    - proposal half-block
    - sign and send proposal half-block
    - agreement half-block
    - sign and send agreement half-block
    Decide to agree a proposal receive trough a pop-up message

--- detoks/src/main/java/nl/tudelft/trustchain/detoks/TestFragment.kt
+++ detoks/src/main/java/nl/tudelft/trustchain/detoks/TestFragment.kt
@@ -1,5 +1,6 @@
 package nl.tudelft.trustchain.detoks
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
 import nl.tudelft.ipv8.IPv8
@@ -35,8 +36,8 @@ class TestFragment : BaseFragment(R.layout.fragment_test) {
     }
 
     private fun createProposal(recipient: Peer) {
-        val transaction = mapOf("msg" to index)
-        debugLog("Proposing block: ${transaction["msg"]} to ${recipient.key.keyToBin().toHex().take(10)}...")
+        val transaction = mapOf("proposal" to index)
+        debugLog("Proposing block: ${transaction["proposal"]} to ${recipient.key.keyToBin().toHex().take(10)}...")
         trustchainCommunity.createProposalBlock(
             BLOCK_TYPE,
             transaction,
@@ -45,6 +46,14 @@ class TestFragment : BaseFragment(R.layout.fragment_test) {
         index += 1
     }
 
+    private fun createAgreement(recipient: Peer, block: TrustChainBlock) {
+        val transaction = mapOf("agreement" to block.transaction["proposal"])
+        debugLog("Agreeing block: ${transaction["agreement"]} to ${recipient.key.keyToBin().toHex().take(10)}...")
+        trustchainCommunity.createAgreementBlock(
+            block,
+            transaction,
+        )
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         community = IPv8Android.getInstance().getOverlay()!!
@@ -59,39 +68,55 @@ class TestFragment : BaseFragment(R.layout.fragment_test) {
             createProposal(ipv8.myPeer)
         }
 
+        trustchainCommunity.registerBlockSigner(BLOCK_TYPE, object : BlockSigner {
+            override fun onSignatureRequest(block: TrustChainBlock) {
+                debugLog("create agreement block")
+                trustchain.createAgreementBlock(block, mapOf("agreement" to block.transaction["proposal"]))
+            }
+        })
+
+
         trustchainCommunity.addListener(BLOCK_TYPE, object : BlockListener {
             override fun onBlockReceived(block: TrustChainBlock) {
 
-                debugLog("Received block ${block.transaction["msg"]}")
-
-                // TODO validate integrity...
-
-                // if valid, create agreement block
-
-            }
-        })
-
-        trustchainCommunity.registerTransactionValidator(BLOCK_TYPE, object : TransactionValidator {
-            override fun validate(
-                block: TrustChainBlock,
-                database: TrustChainStore
-            ): ValidationResult {
-
-                if (block.isProposal) {
-                    debugLog("received a proposal to validate from ${block.publicKey.toHex().take(10)}...")
-                    return ValidationResult.Valid
+                if  (block.isProposal) {
+                    debugLog("Received proposal block for transaction ${block.transaction["proposal"]}")
+                    val builder = AlertDialog.Builder(requireContext())
+                    builder.setTitle("Proposal Block received!")
+                    builder.setMessage("Do you want to confirm it?")
+                    builder.setPositiveButton("Yes") { _, _ ->
+                        createAgreement(ipv8.myPeer, block)
+                    }
+                    val dialog= builder.create()
+                    dialog.show()
+                }else if (block.isAgreement) {
+                    debugLog("Received agreement block for transaction ${block.transaction["agreement"]}")
                 }
-
-                return ValidationResult.Invalid(listOf("Unexpected block"));
             }
         })
+
+//        trustchainCommunity.registerTransactionValidator(BLOCK_TYPE, object : TransactionValidator {
+//            override fun validate(
+//                block: TrustChainBlock,
+//                database: TrustChainStore
+//            ): ValidationResult {
+//
+//                if (block.isProposal) {
+//                    debugLog("received a proposal to validate from ${block.publicKey.toHex().take(10)}...")
+//                    return ValidationResult.Valid
+//                }
+//
+//                return ValidationResult.Invalid(listOf("Unexpected block"));
+//            }
+//        })
 
         trustchainCommunity.registerBlockSigner(BLOCK_TYPE, object : BlockSigner {
             override fun onSignatureRequest(block: TrustChainBlock) {
                 debugLog("create agreement block")
-                trustchain.createAgreementBlock(block, block.transaction)
+                trustchain.createAgreementBlock(block, mapOf("agreement" to block.transaction["proposal"]))
             }
         })
+
 
 
     }
