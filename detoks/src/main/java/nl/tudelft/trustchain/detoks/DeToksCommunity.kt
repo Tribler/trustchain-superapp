14d2cb26949ead2b83a01875b3a9becd3b4831c1
Chris Bras <chris.s.bras@gmail.com>
2023-03-13 00:44:35 +0100

    merged master

--- detoks/src/main/java/nl/tudelft/trustchain/detoks/DeToksCommunity.kt
+++ detoks/src/main/java/nl/tudelft/trustchain/detoks/DeToksCommunity.kt
@@ -1,14 +1,16 @@
 package nl.tudelft.trustchain.detoks
 
+import android.content.Context
 import android.util.Log
 import nl.tudelft.ipv8.Community
-import nl.tudelft.ipv8.IPv4Address
+import nl.tudelft.ipv8.Overlay
+import nl.tudelft.ipv8.Peer
 import nl.tudelft.ipv8.messaging.Packet
 
-class DeToksCommunity() : Community() {
+class DeToksCommunity(private val context: Context) : Community() {
 
     init {
-        messageHandlers[MESSAGE_TORRENT_ID] = ::onMessage
+        messageHandlers[MESSAGE_TORRENT_ID] = ::onGossip
     }
 
     companion object {
@@ -17,24 +19,32 @@ class DeToksCommunity() : Community() {
 
     override val serviceId = "c86a7db45eb3563ae047639817baec4db2bc7c25"
 
-    override fun walkTo(address: IPv4Address) {
-        super.walkTo(address)
+    fun gossipWith(peer: Peer) {
+        Log.d("DeToksCommunity", "Gossiping with ${peer.mid}, address: ${peer.address}")
 
         Log.d("DeToksCommunity", this.getPeers().toString())
         Log.d("DeToksCommunity", this.myPeer.toString())
-        broadcastTorrent() // FOR TESTING PURPOSES
+        val listOfTorrents = TorrentManager.getInstance(context).getListOfTorrents()
+        if(listOfTorrents.isEmpty()) return
+        val magnet = listOfTorrents.random().makeMagnetUri()
+        val packet = serializePacket(MESSAGE_TORRENT_ID, TorrentMessage(magnet))
+
+        send(peer.address, packet)
     }
 
-    fun broadcastTorrent() {
-        for (peer in getPeers()) {
-            Log.d("DeToksCommunity", "Sending torrent to $peer")
-            val packet = serializePacket(MESSAGE_TORRENT_ID, TorrentMessage("This is a torrent!"))
-            send(peer.address, packet)
-        }
-    }
-
-    private fun onMessage(packet: Packet) {
+    private fun onGossip(packet: Packet) {
         val (peer, payload) = packet.getAuthPayload(TorrentMessage.Deserializer)
-        Log.d("DeToksCommunity", peer.mid + ": " + payload.message)
+        val torrentManager = TorrentManager.getInstance(context)
+        Log.d("DeToksCommunity", "received torrent from ${peer.mid}, address: ${peer.address}, magnet: ${payload.magnet}")
+
+        torrentManager.addTorrent(payload.magnet)
+    }
+
+    class Factory(
+        private val context: Context
+    ) : Overlay.Factory<DeToksCommunity>(DeToksCommunity::class.java) {
+        override fun create(): DeToksCommunity {
+            return DeToksCommunity(context)
+        }
     }
 }
