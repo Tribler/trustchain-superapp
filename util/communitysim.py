663407cfc1fd02697df2601fcf8aac19f62aa511
Chris Bras <chris.s.bras@gmail.com>
2023-03-13 00:42:38 +0100

    added script to summon infinte nodes

--- util/communitysim.py
+++ util/communitysim.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from asyncio import ensure_future, get_event_loop
 
 from pyipv8.ipv8.community import Community
@@ -11,11 +12,57 @@ from pyipv8.ipv8.configuration import (
     Bootstrapper,
 )
 
-# from pyipv8.ipv8.bootstrapping.dispersy.bootstrapper import DispersyBootstrapper
 from pyipv8.ipv8_service import IPv8
 
+bootstrap_config = {
+    "class": "DispersyBootstrapper",
+    "init": {
+        "ip_addresses": [
+            ("130.161.119.206", 6421),
+            ("130.161.119.206", 6422),
+            ("131.180.27.155", 6423),
+            ("131.180.27.156", 6424),
+            ("131.180.27.161", 6427),
+            ("131.180.27.161", 6521),
+            ("131.180.27.161", 6522),
+            ("131.180.27.162", 6523),
+            ("131.180.27.162", 6524),
+            ("130.161.119.215", 6525),
+            ("130.161.119.215", 6526),
+            ("130.161.119.201", 6527),
+            ("130.161.119.201", 6528),
+            ("131.180.27.188", 1337),
+            ("131.180.27.187", 1337),
+            ("131.180.27.161", 6427),
+        ],
+        "dns_addresses": [
+            ("dispersy1.tribler.org", 6421),
+            ("dispersy1.st.tudelft.nl", 6421),
+            ("dispersy2.tribler.org", 6422),
+            ("dispersy2.st.tudelft.nl", 6422),
+            ("dispersy3.tribler.org", 6423),
+            ("dispersy3.st.tudelft.nl", 6423),
+            ("dispersy4.tribler.org", 6424),
+            ("tracker1.ip-v8.org", 6521),
+            ("tracker2.ip-v8.org", 6522),
+            ("tracker3.ip-v8.org", 6523),
+            ("tracker4.ip-v8.org", 6524),
+            ("tracker5.ip-v8.org", 6525),
+            ("tracker6.ip-v8.org", 6526),
+            ("tracker7.ip-v8.org", 6527),
+            ("tracker8.ip-v8.org", 6528),
+        ],
+        "bootstrap_timeout": 0.5,
+    },
+}
 
-class MyCommunity(Community):
+
+bootstrapper = [
+    BootstrapperDefinition(Bootstrapper.DispersyBootstrapper, bootstrap_config["init"])
+]
+
+
+class DetoksCommunity(Community):
     community_id = bytes.fromhex("c86a7db45eb3563ae047639817baec4db2bc7c25")
 
     def started(self):
@@ -24,27 +71,54 @@ class MyCommunity(Community):
                 "I am:", self.my_peer, "\nI know:", [str(p) for p in self.get_peers()]
             )
 
+        async def gossip():
+            """
+            Function that runs the gossip message like the android app
+            """
+
         # We register a asyncio task with this overlay.
         # This makes sure that the task ends when this overlay is unloaded.
         # We call the 'print_peers' function every 5.0 seconds, starting now.
         self.register_task("print_peers", print_peers, interval=5.0, delay=0)
+        self.register_task("gossip", print_peers, interval=5.0, delay=0)
+
+        self.add_message_handler(1, self.on_message)
+
+    def on_message(self, peer, payload):
+        pass
+        # print("Got a message from:", peer)
+        # print("The message includes the first payload:\n", payload)
+        # print("The message includes the second payload:\n", payload2)
 
 
-async def start_communities():
-    for i in [1]:
+async def start_nodes(num_nodes):
+    if not os.path.exists("keys/"):
+        os.mkdir("keys/")
+
+    for i in range(num_nodes):
+        node_name = f"dummy_peer_{i}"
         builder = ConfigBuilder().clear_keys().clear_overlays()
-        builder.add_key("my peer", "medium", f"ec{i}.pem")
+        builder.add_key(node_name, "medium", f"keys/ec{i}.pem")
         builder.add_overlay(
-            "MyCommunity",
-            "my peer",
-            [WalkerDefinition(Strategy.RandomWalk, 20, {"timeout": 30.0})],
-            default_bootstrap_defs,
+            "DeToksCommunity",
+            node_name,
+            [WalkerDefinition(Strategy.RandomWalk, 10, {"timeout": 0.5})],
+            bootstrapper,
             {},
             [("started",)],
         )
-        ipv8 = IPv8(builder.finalize(), extra_communities={"MyCommunity": MyCommunity})
+        ipv8 = IPv8(
+            builder.finalize(), extra_communities={"DeToksCommunity": DetoksCommunity}
+        )
         await ipv8.start()
 
 
-ensure_future(start_communities())
-get_event_loop().run_forever()
+if __name__ == "__main__":
+    try:
+        num_nodes = int(sys.argv[1])
+    except:
+        print("Please provide a number of nodes to spawn")
+        exit(1)
+
+    ensure_future(start_nodes(num_nodes))
+    get_event_loop().run_forever()
