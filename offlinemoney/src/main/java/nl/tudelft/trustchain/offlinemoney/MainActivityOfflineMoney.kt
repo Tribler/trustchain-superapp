610931b753c6eec2574de28d1c491d3014c4cd77
Adrian-Tudor Dumitrescu <57295921+AdiDumi@users.noreply.github.com>
2023-03-15 12:23:47 +0100

    added first basic app hello world

---
+++ offlinemoney/src/main/java/nl/tudelft/trustchain/offlinemoney/MainActivityOfflineMoney.kt
@@ -0,0 +1,11 @@
+package nl.tudelft.trustchain.offlinemoney
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityOfflineMoney : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_offline_money)
+    }
+}
