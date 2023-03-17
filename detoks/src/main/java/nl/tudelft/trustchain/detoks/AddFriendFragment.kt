8a0bae8f341461f7bc119cd1f5cfd1266cab326f
Ne184 <64256244+Ne184@users.noreply.github.com>
2023-03-17 19:41:19 +0100

    Add show QR code of public key and read results

--- detoks/src/main/java/nl/tudelft/trustchain/detoks/AddFriendFragment.kt
+++ detoks/src/main/java/nl/tudelft/trustchain/detoks/AddFriendFragment.kt
@@ -1,12 +1,23 @@
 package nl.tudelft.trustchain.detoks
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import nl.tudelft.trustchain.common.ui.BaseFragment
 import nl.tudelft.trustchain.common.util.QRCodeUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import androidx.lifecycle.lifecycleScope
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,7 +29,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [AddFriendFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AddFriendFragment : Fragment() {
+class AddFriendFragment : BaseFragment(R.layout.fragment_add_friend) {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -50,8 +61,47 @@ class AddFriendFragment : Fragment() {
         buttonScan.setOnClickListener {
             qrCodeUtils.startQRScanner(this, null, true)
         }
+
+        val My_QR = view.findViewById<ImageView>(R.id.My_QR)
+
+        val buttonShow = view.findViewById<Button>(R.id.button_show)
+        buttonShow.setOnClickListener {
+            buttonScan.visibility = View.INVISIBLE
+            val myPublicKey = getIpv8().myPeer.publicKey.pub().keyToBin().toString()
+            lifecycleScope.launch {
+                var bitmap = withContext(Dispatchers.Default) {
+                    // qrCodeUtils.createQR(payload.serialize().toHex())
+                    qrCodeUtils.createQR(myPublicKey)
+                }
+                My_QR.visibility = View.VISIBLE
+                My_QR.setImageBitmap(bitmap)
+
+            }
+        }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val content = qrCodeUtils.parseActivityResult(requestCode,resultCode,data)
+        Log.v("PublicKey content", content.toString())
+
+        val nameFriend = view?.findViewById<EditText>(R.id.name)
+        nameFriend?.visibility = View.VISIBLE
+
+        val buttonSave = view?.findViewById<Button>(R.id.save_friend)
+        buttonSave?.visibility = View.VISIBLE
+
+        buttonSave?.setOnClickListener{
+            val username = nameFriend?.text
+           if(nameFriend?.text == null){
+               Toast.makeText(this.context,"Enter friend's name!", Toast.LENGTH_LONG).show()
+           } else {
+               //save call to db
+               Log.v("Save pub key", content.toString())
+               Log.v("Name ", username.toString())
+           }
+        }
+
+    }
 
     companion object {
         /**
