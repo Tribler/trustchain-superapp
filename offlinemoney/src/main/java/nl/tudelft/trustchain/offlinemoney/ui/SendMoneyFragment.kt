79e59b0559556fb4f74319ba78d0563adcdb9398
Gerben Timmerman <gerbentimmerman@users.noreply.github.com>
2023-03-17 15:17:16 +0100

    Created QR encoder
    Still not fully functional

---
+++ offlinemoney/src/main/java/nl/tudelft/trustchain/offlinemoney/ui/SendMoneyFragment.kt
@@ -0,0 +1,49 @@
+package nl.tudelft.trustchain.offlinemoney.ui
+
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import nl.tudelft.trustchain.common.util.QRCodeUtils
+import nl.tudelft.trustchain.common.util.viewBinding
+import nl.tudelft.trustchain.offlinemoney.R
+import nl.tudelft.trustchain.offlinemoney.databinding.SendMoneyFragmentBinding
+import org.json.JSONObject
+
+class SendMoneyFragment  : OfflineMoneyBaseFragment(R.layout.send_money_fragment)  {
+    private val binding by viewBinding(SendMoneyFragmentBinding::bind)
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // val exchange_list = mutableListOf("public_key" , "private_key", 5);
+
+        val exchange_json_object = requireArguments().getString(ARG_DATA)!!
+        println(exchange_json_object)
+        binding.txtSendData.text = exchange_json_object
+
+        // binding.requestJsonData.text = exchange_json_object
+
+        lifecycleScope.launch {
+            val bitmap = withContext(Dispatchers.Default) {
+                qrCodeUtils.createQR(exchange_json_object)
+            }
+            binding.qrImageView.setImageBitmap(bitmap)
+        }
+
+        binding.btnContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_sendMoneyFragment_to_tranferFragment)
+        }
+    }
+
+    private val qrCodeUtils by lazy {
+        QRCodeUtils(requireContext())
+    }
+
+    companion object {
+        const val ARG_DATA = "data"
+    }
+}
--- offlinemoney/src/main/java/nl/tudelft/trustchain/offlinemoney/ui/TransferFragment.kt
+++ offlinemoney/src/main/java/nl/tudelft/trustchain/offlinemoney/ui/TransferFragment.kt
@@ -2,10 +2,14 @@ package nl.tudelft.trustchain.offlinemoney.ui
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
+import nl.tudelft.ipv8.util.toHex
+import nl.tudelft.trustchain.common.contacts.ContactStore
 import nl.tudelft.trustchain.common.util.QRCodeUtils
 import nl.tudelft.trustchain.common.util.viewBinding
 import nl.tudelft.trustchain.offlinemoney.R
 import nl.tudelft.trustchain.offlinemoney.databinding.ActivityMainOfflineMoneyBinding
+import org.json.JSONObject
 
 class TransferFragment : OfflineMoneyBaseFragment(R.layout.activity_main_offline_money) {
     private val binding by viewBinding(ActivityMainOfflineMoneyBinding::bind)
@@ -24,5 +28,40 @@ class TransferFragment : OfflineMoneyBaseFragment(R.layout.activity_main_offline
         binding.btnGet.setOnClickListener {
             qrCodeUtils.startQRScanner(this)
         }
+
+        binding.btnSend.setOnClickListener{
+            val amount = getAmount(binding.edtAmount.text.toString())
+            println(amount)
+
+            if (amount > 0) {
+                val myPeer = "kjalsdjfoiawonsad"
+
+                val connectionData = JSONObject()
+                connectionData.put("public_key", myPeer)
+                connectionData.put("amount", amount)
+                connectionData.put("name", myPeer)
+
+
+                val args = Bundle()
+
+                args.putString(SendMoneyFragment.ARG_DATA, connectionData.toString())
+
+                findNavController().navigate(
+                    R.id.action_transferFragment_to_sendMoneyFragment,
+                    args
+                )
+            }
+
+        }
+
+    }
+
+    fun getAmount(amount: String): Long {
+        val regex = """[^\d]""".toRegex()
+        if (amount.isEmpty()) {
+            return 0L
+        }
+        return regex.replace(amount, "").toLong()
     }
 }
+
