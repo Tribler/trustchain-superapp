d91cf766262cbc6ef38214611c56d54785ca8d09
Robert-ML <37192540+Robert-ML@users.noreply.github.com>
2023-03-18 17:01:44 +0100

    finished RequestPayload and Promise but not tested

--- offlinemoney/src/main/java/nl/tudelft/trustchain/offlinemoney/payloads/RequestPayload.kt
+++ offlinemoney/src/main/java/nl/tudelft/trustchain/offlinemoney/payloads/RequestPayload.kt
@@ -1,8 +1,14 @@
 package nl.tudelft.trustchain.offlinemoney.payloads
 
-import org.json.JSONObject
-
+import nl.tudelft.ipv8.keyvault.CryptoProvider
+import nl.tudelft.ipv8.keyvault.JavaCryptoProvider
 import nl.tudelft.ipv8.keyvault.PublicKey
+import nl.tudelft.ipv8.util.hexToBytes
+import nl.tudelft.ipv8.util.toHex
+
+import android.util.Log
+
+import org.json.JSONObject
 
 class RequestPayload(
     private val pbk: PublicKey,
@@ -10,7 +16,7 @@ class RequestPayload(
     fun toJson() :JSONObject {
         var ret = JSONObject()
 
-        ret.put(field_pbk, pbk)
+        ret.put(field_pbk, pbk.keyToBin().toHex())
 
         return ret
     }
@@ -18,6 +24,29 @@ class RequestPayload(
     companion object {
         // public key
         const val field_pbk: String = "PUB_K"
+
+        var defaultCryptoProvider: CryptoProvider = JavaCryptoProvider
+
+        fun fromJson(json: JSONObject): RequestPayload? {
+//            first check if all fields are present
+            if (!json.has(field_pbk)) {
+                Log.d("WARN", "In 'RequestPayload::fromJson', json has missing field")
+                return null
+            }
+
+            return try {
+                val pbk: PublicKey = defaultCryptoProvider.keyFromPublicBin(
+                    json.getString(field_pbk).hexToBytes()
+                )
+
+                RequestPayload(
+                    pbk
+                )
+            } catch (e: Exception) {
+                Log.d("WARN", "Exception in 'RequestPayload::fromJson': $e")
+                null
+            }
+        }
     }
 }
 
