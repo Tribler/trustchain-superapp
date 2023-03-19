c72d074ff63af39d1cb743f19d4a2af6217afefd
Adrian-Tudor Dumitrescu <57295921+AdiDumi@users.noreply.github.com>
2023-03-19 10:57:33 +0100

    added navigation menu

--- offlinemoney/src/main/java/nl/tudelft/trustchain/offlinemoney/MainActivityOfflineMoney.kt
+++ offlinemoney/src/main/java/nl/tudelft/trustchain/offlinemoney/MainActivityOfflineMoney.kt
@@ -4,4 +4,5 @@ import nl.tudelft.trustchain.common.BaseActivity
 
 class MainActivityOfflineMoney() : BaseActivity() {
     override val navigationGraph = R.navigation.nav_graph_offlinemoney
+    override val bottomNavigationMenu = R.menu.offlinemoney_menu
 }
