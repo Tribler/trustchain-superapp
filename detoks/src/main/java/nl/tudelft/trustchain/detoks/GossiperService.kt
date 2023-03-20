d329780db737458a1b2b5f4424d42ff1044acd82
AlexDumitru17 <a.dumitru@student.tudelft.nl>
2023-03-20 02:28:55 +0100

    Added an example of a trustchain transaction

--- detoks/src/main/java/nl/tudelft/trustchain/detoks/GossiperService.kt
+++ detoks/src/main/java/nl/tudelft/trustchain/detoks/GossiperService.kt
@@ -4,9 +4,11 @@ import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import android.util.Log
 import kotlinx.coroutines.*
 import nl.tudelft.ipv8.Peer
 import nl.tudelft.ipv8.android.IPv8Android
+import nl.tudelft.trustchain.common.util.TrustChainHelper
 import kotlin.system.exitProcess
 
 class GossiperService : Service() {
@@ -40,12 +42,25 @@ class GossiperService : Service() {
     }
 
     private suspend fun gossipTorrents() {
+
         while (scope.isActive) {
             val deToksCommunity = IPv8Android.getInstance().getOverlay<DeToksCommunity>()!!
-
+            var trustchain = TrustChainHelper(deToksCommunity)
             val randomPeer = pickRandomPeer(deToksCommunity)
-            if(randomPeer != null)
+            if(randomPeer != null) {
+                Log.d("DetoksCommunity", "trustchain users " +  trustchain.getUsers())
                 deToksCommunity.gossipWith(randomPeer)
+
+                deToksCommunity.database.getUsers()
+                trustchain.createTxProposalBlock(1.0f, randomPeer.publicKey.keyToBin())
+                Log.d("DetoksCommunity", "trustchain size " +  deToksCommunity.getChainLength())
+                Log.d("DeToksCommunity",  "peers size" + deToksCommunity.getPeers().size )
+                Log.d("DeToksCommunity", "users in trustchain" + deToksCommunity.database.getUsers())
+                Log.d("DeToksCommunity", "blocks" + deToksCommunity.database.getAllBlocks())
+
+            }
+
+
 
             delay(4000)
         }
