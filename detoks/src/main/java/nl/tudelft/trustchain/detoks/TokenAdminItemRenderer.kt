8d67a8ecfff65533a52531ae932cc57aec300c0e
julio1998 <julrvsio@hotmail.com>
2023-03-28 16:18:09 +0200

    Enhanced item token

--- detoks/src/main/java/nl/tudelft/trustchain/detoks/TokenAdminItemRenderer.kt
+++ detoks/src/main/java/nl/tudelft/trustchain/detoks/TokenAdminItemRenderer.kt
@@ -2,13 +2,19 @@ package nl.tudelft.trustchain.detoks
 
 import android.view.View
 import com.mattskala.itemadapter.ItemLayoutRenderer
+import kotlinx.android.synthetic.main.item_token_admin.view.*
 
 class TokenAdminItemRenderer() : ItemLayoutRenderer<TokenAdminItem, View>(
     TokenAdminItem::class.java
 ){
 
     override fun bindView(item: TokenAdminItem, view: View) = with(view) {
-        // doSomeStuff
+        valueToken.text = "0"
+        latestTimestamp.text = ""
+        currentOwner.text = ""
+
+        //link verify
+        //link history button
     }
 
     override fun getLayoutResourceId(): Int {
