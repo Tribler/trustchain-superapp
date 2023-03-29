8a2bd1c1e35de41b55111ee19c47e7c250311613
julio1998 <julrvsio@hotmail.com>
2023-03-29 10:31:46 +0200

    Created function that connects button to item

--- detoks/src/main/java/nl/tudelft/trustchain/detoks/TokenAdminItemRenderer.kt
+++ detoks/src/main/java/nl/tudelft/trustchain/detoks/TokenAdminItemRenderer.kt
@@ -1,10 +1,19 @@
 package nl.tudelft.trustchain.detoks
 
+import Wallet
 import android.view.View
 import com.mattskala.itemadapter.ItemLayoutRenderer
 import kotlinx.android.synthetic.main.item_token_admin.view.*
 
-class TokenAdminItemRenderer() : ItemLayoutRenderer<TokenAdminItem, View>(
+interface TokenButtonListener {
+    fun onVerifyClick(token: Token)
+    fun onHistoryClick(token: Token)
+}
+
+class TokenAdminItemRenderer(
+    private val wallet: Wallet?,
+    private val displayAs: String,
+    private val listener: TokenButtonListener) : ItemLayoutRenderer<TokenAdminItem, View>(
     TokenAdminItem::class.java
 ){
 
--- detoks/src/main/java/nl/tudelft/trustchain/detoks/TokenListAdminFragment.kt
+++ detoks/src/main/java/nl/tudelft/trustchain/detoks/TokenListAdminFragment.kt
@@ -1,5 +1,6 @@
 package nl.tudelft.trustchain.detoks
 
+import Wallet
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,11 +11,13 @@ import androidx.lifecycle.liveData
 import androidx.navigation.findNavController
 import com.mattskala.itemadapter.Item
 import com.mattskala.itemadapter.ItemAdapter
+import nl.tudelft.ipv8.keyvault.PrivateKey
 import nl.tudelft.trustchain.common.ui.BaseFragment
 
-class TokenListAdminFragment : BaseFragment(R.layout.fragment_token_list_admin)  {
+class TokenListAdminFragment : BaseFragment(R.layout.fragment_token_list_admin), TokenButtonListener  {
 
     private val adapter = ItemAdapter()
+    val myPublicKey = getIpv8().myPeer.publicKey
 
     private val items: LiveData<List<Item>> by lazy {
         liveData { emit(listOf<Item>()) }
@@ -29,7 +32,10 @@ class TokenListAdminFragment : BaseFragment(R.layout.fragment_token_list_admin) 
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        adapter.registerRenderer(TokenAdminItemRenderer())
+        val user = "admin"
+        val wallet = view?.let { Wallet.getInstance(it.context, myPublicKey, getIpv8().myPeer.key as PrivateKey) }
+
+        adapter.registerRenderer(TokenAdminItemRenderer(wallet, user, this))
 
         return inflater.inflate(R.layout.fragment_token_list_admin, container, false)
     }
@@ -42,6 +48,14 @@ class TokenListAdminFragment : BaseFragment(R.layout.fragment_token_list_admin) 
             val navController = view.findNavController()
             navController.navigate(R.id.tokenListAdmin)
         }
+    }
+
+    override fun onHistoryClick(token: Token) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onVerifyClick(token: Token) {
+        TODO("Not yet implemented")
     }
 
     companion object {
