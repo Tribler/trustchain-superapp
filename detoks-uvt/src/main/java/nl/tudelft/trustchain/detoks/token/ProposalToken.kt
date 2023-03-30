0d7c9cf1480868f057753c606334fb3711641c48
Bryan He <B.Y.He@student.tudelft.nl>
2023-03-30 20:33:07 +0200

    removed faulty second call of createProposalToken()

--- detoks-uvt/src/main/java/nl/tudelft/trustchain/detoks/token/ProposalToken.kt
+++ detoks-uvt/src/main/java/nl/tudelft/trustchain/detoks/token/ProposalToken.kt
@@ -22,12 +22,13 @@ class ProposalToken {
      * anyone who gives the video X a like/upvote.
      * (except the peer that initiated the proposal block)
      */
-    private fun createProposalToken(): TrustChainBlock? {
+    private fun createProposalToken(magnetURI: String): TrustChainBlock? {
         val upvoteCommunity = IPv8Android.getInstance().getOverlay<UpvoteCommunity>()
         val myPeer = IPv8Android.getInstance().myPeer
 
         val transaction = mapOf(
             "videoID" to "TODO: REPLACE THIS WITH ACTUAL VIDEO ID",
+            "magnetURI" to magnetURI,
             "heartTokenGivenBy" to ANY_COUNTERPARTY_PK.toHex(),
             "heartTokenGivenTo" to myPeer.publicKey.keyToBin().toHex()
         )
@@ -42,12 +43,25 @@ class ProposalToken {
     /**
      * Sets a listener to create a proposal token after a long press has been detected.
      * We do not have a post video functionality yet
-     * A long press of 2 seconds represents/simulates a peer having posted a video
+     * Clicking the button represents/simulates a peer having posted a video
      */
     fun setPostVideoListener(proposalSendButton: Button, itemView: View, torrentManager: TorrentManager) {
         proposalSendButton.setOnClickListener{
-            val proposalBlock = createProposalToken()
-            val upvoteCommunity = IPv8Android.getInstance().getOverlay<UpvoteCommunity>()
+            val torrentInfo = torrentManager.getSeedableTorrents().get(0)
+            val magnetURI = torrentManager.seedTorrent(torrentInfo)
+            if (magnetURI == null) {
+                Log.i("DeToks", "Seeding failed!")
+                Log.i("DeToks", "failed to post video")
+                Toast.makeText(
+                    itemView.context,
+                    "failed to make a proposal block because seeding failed & magnetURI is null",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+            Log.i("DeToks", "Seeding succeeded!")
+            val proposalBlock = createProposalToken(magnetURI)
+            val upvoteCommunity = IPv8Android.getInstance().getOverlay<UpvoteCommunity>()!!
             val hash = proposalBlock?.calculateHash()!!
             val myPeer = IPv8Android.getInstance().myPeer
             val message = "Button Clicked! Your public key: " +
@@ -56,27 +70,13 @@ class ProposalToken {
                 "The hash of this block is ${hash.toHex()}, corresponding hashCode is: ${hash.hashCode()} \n" +
                 "the block Id of this proposal block is: ${proposalBlock.blockId} \n" +
                 "the linked block id is: ${proposalBlock.linkedBlockId}\n"
-//            torrentManager.addNewVideo(hash.toHex(), proposalBlock.timestamp.toString(), proposalBlock.blockId)
-            val torrentInfo = torrentManager.getSeedableTorrents().get(0)
-            val magnetURI = torrentManager.seedTorrent(torrentInfo)
-
-            if (magnetURI == null) {
-                Log.i("DeToks", "Seeding failed!")
-            } else {
-                Log.i("DeToks", "Seeding succeeded!")
-            }
-
-            if (magnetURI != null) {
-                upvoteCommunity?.sendVideoData(magnetURI, hash.toHex())
-            }
-
+            upvoteCommunity.sendVideoData(magnetURI, hash.toHex())
             Log.i("DeToks", message)
             Toast.makeText(
                 itemView.context,
                 message,
                 Toast.LENGTH_SHORT
             ).show()
-            createProposalToken()
         }
     }
 }
