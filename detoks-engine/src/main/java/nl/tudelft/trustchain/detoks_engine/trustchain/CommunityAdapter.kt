197daa70bb47700b625d09e1879d5d0b31f4a257
Bob Brockbernd <bobbrockbernd@gmail.com>
2023-04-11 11:45:40 +0200

    bugfixes trustchain

--- detoks-engine/src/main/java/nl/tudelft/trustchain/detoks_engine/trustchain/CommunityAdapter.kt
+++ detoks-engine/src/main/java/nl/tudelft/trustchain/detoks_engine/trustchain/CommunityAdapter.kt
@@ -7,6 +7,7 @@ import nl.tudelft.ipv8.attestation.trustchain.TrustChainBlock
 import nl.tudelft.ipv8.attestation.trustchain.TrustChainCommunity
 import nl.tudelft.trustchain.common.util.TrustChainHelper
 import nl.tudelft.trustchain.detoks_engine.manage_tokens.Transaction
+import kotlin.math.min
 
 class CommunityAdapter(
     private val trustChainCommunity: TrustChainCommunity
@@ -16,8 +17,8 @@ class CommunityAdapter(
     val logger = KotlinLogging.logger("TokenTransaction")
     lateinit var recvAckHandler: ((transactionId: String) -> Unit)
     lateinit var recvTransactionHandler: ((transaction: Transaction) -> Unit)
-    private var firstRelevantBlockSeq = -1
-    private var tokenPosInBlock = -1
+    private var blockPointer = -1
+    private var tokenInBlockPointer = -1
     private var tokenCount = 0
 
     init {
@@ -31,8 +32,8 @@ class CommunityAdapter(
         var lastSentToken: String? = null
         var block = trustChainCommunity.database.getLatest(myPublicKey, TOKEN_BLOCK_TYPE)
         while(block != null) {
-            // Agreement with my public key is a sent token block
-            if (block.isAgreement && !block.isSelfSigned) {
+            // Proposal with my public key is a sent token block
+            if (block.isProposal && !block.isSelfSigned) {
                 lastSentToken = Transaction.fromTrustChainTransactionObject(block.transaction).tokens.last()
                 break
             }
@@ -44,36 +45,105 @@ class CommunityAdapter(
         block = trustChainCommunity.database.getLatest(myPublicKey, TOKEN_BLOCK_TYPE)
         while (block != null) {
             // Agreement with my public key AS LINK KEY is a rec token block
-            if (block.isProposal) {
+            if (block.isAgreement) {
+                blockPointer = block.sequenceNumber.toInt()
+                tokenInBlockPointer = 0
                 if (lastSentToken != null && Transaction.fromTrustChainTransactionObject(block.transaction).tokens.contains(lastSentToken)) {
-                    firstRelevantBlockSeq = block.sequenceNumber.toInt()
-                    tokenPosInBlock = Transaction.fromTrustChainTransactionObject(block.transaction).tokens.indexOf(lastSentToken)
-                    tokenCount += Transaction.fromTrustChainTransactionObject(block.transaction).tokens.size - tokenPosInBlock
+                    tokenInBlockPointer = Transaction.fromTrustChainTransactionObject(block.transaction).tokens.indexOf(lastSentToken)
+                    tokenCount += Transaction.fromTrustChainTransactionObject(block.transaction).tokens.size - tokenInBlockPointer
+                    popToken()
                     break
                 }
                 tokenCount += Transaction.fromTrustChainTransactionObject(block.transaction).tokens.size
             }
             block = trustChainCommunity.database.getBlockBefore(block, TOKEN_BLOCK_TYPE)
         }
-
         logger.debug("test")
     }
 
     private fun handleBlock(block: TrustChainBlock) {
-        logger.debug("Block token: ${block.transaction}, is proposal: ${block.isProposal}, is agreement: ${block.isAgreement}, PK til 8: ${block.publicKey.toString().substring(0, 8)}")
-        if (block.isProposal ) {
+        logger.debug("Block token received: ${block.transaction}, is proposal: ${block.isProposal}, is agreement: ${block.isAgreement}, PK til 8: ${block.publicKey.toString().substring(0, 8)}, is self signed ${block.isSelfSigned}")
+
+        // A proposal block for me (so i receive tokens), action is to agree
+        if (block.isProposal && (block.isSelfSigned || !block.publicKey.contentEquals(myPublicKey))) {
+            agreeTransaction(block)
+        }
+
+        // Agreement block signed by me means i accept the tokens sent to me
+        else if (block.isAgreement && block.publicKey.contentEquals(myPublicKey)) {
             recvTransactionHandler(Transaction.fromTrustChainTransactionObject(block.transaction))
-            if (!block.publicKey.contentEquals(myPublicKey)) {
-                agreeTransaction(block)
+            val trans = Transaction.fromTrustChainTransactionObject(block.transaction)
+            tokenCount += trans.tokens.size
+            if (blockPointer == -1) {
+                blockPointer = block.sequenceNumber.toInt()
+                tokenInBlockPointer = 0
             }
-        } else if (block.isAgreement && !block.publicKey.contentEquals(myPublicKey)) {
-            recvAckHandler(Transaction.fromTrustChainTransactionObject(block.transaction).transactionId)
+        }
+
+        // TODO check agreement block of other party
+//        // Other party accepted my proposal
+//        else if (block.isProposal && block.publicKey.contentEquals(myPublicKey)) {
+//            tokenCount -= Transaction.fromTrustChainTransactionObject(block.transaction).tokens.size
+//            recvAckHandler(Transaction.fromTrustChainTransactionObject(block.transaction).transactionId)
+//        }
+    }
+
+    fun sendTokens(amount: Int, peer: Peer) {
+        if (tokenCount > 0) {
+            val nToSend = min(amount, tokenCount)
+            val tokens = mutableListOf<String>()
+            repeat(nToSend) {
+                tokens.add(popToken()!!)
+            }
+            proposeTransaction(Transaction(tokens), peer)
         }
     }
 
-    fun proposeTransaction(transaction: Transaction, peer: Peer) {
+    private fun popToken() : String? {
+        if (tokenCount > 0) {
+            // Fetch token at pointer
+            val block = trustChainCommunity.database.get(myPublicKey, blockPointer.toUInt())!!
+            val transaction = Transaction.fromTrustChainTransactionObject(block.transaction)
+            val token = transaction.tokens[tokenInBlockPointer]
+
+            // Decrement token count
+            tokenCount--
+
+            // If last token set pointers to -1 and return
+            if (tokenCount == 0) {
+                blockPointer = -1
+                tokenInBlockPointer = -1
+                return token
+            }
+
+            // I at end of block search for next one
+            if (tokenInBlockPointer == transaction.tokens.size - 1) {
+                tokenInBlockPointer = 0
+                var nextBlock = trustChainCommunity.database.getBlockAfter(block, TOKEN_BLOCK_TYPE)
+
+                while (!nextBlock!!.isAgreement) {
+                    nextBlock = trustChainCommunity.database.getBlockAfter(nextBlock, TOKEN_BLOCK_TYPE)
+                }
+                blockPointer = nextBlock.sequenceNumber.toInt()
+
+            // else only move in block pointer
+            } else {
+                tokenInBlockPointer++
+            }
+            return token
+        }
+        return null
+    }
+
+    private fun proposeTransaction(transaction: Transaction, peer: Peer) {
         logger.debug("Proposing transaction: ${transaction.transactionId} with first token: ${transaction.tokens[0]}")
         trustChainCommunity.createProposalBlock(TOKEN_BLOCK_TYPE, transaction.toTrustChainTransaction(), peer.publicKey.keyToBin())
+    }
+
+    fun injectTokens(tokens: List<String>) {
+        logger.debug{"Starting token injection for $tokens"}
+        val transaction = Transaction(tokens)
+        trustChainCommunity.createProposalBlock(TOKEN_BLOCK_TYPE, transaction.toTrustChainTransaction(), myPublicKey)
     }
 
     fun setReceiveAckHandler(handler: ((transactionId: String) -> Unit)) {
@@ -95,9 +165,20 @@ class CommunityAdapter(
         return trustChainCommunity.getPeers()
     }
 
-//    fun getTokens(): List<String> {
-//        trustChainCommunity.database.
-//    }
+    fun getTokens(): List<String> {
+        val tokens = mutableListOf<String>()
+        if (tokenCount == 0) return tokens
+
+        var currentBlock: TrustChainBlock? = trustChainCommunity.database.get(myPublicKey, blockPointer.toUInt())
+        while (tokens.size < tokenCount) {
+            currentBlock!!
+            if (currentBlock.isAgreement) {
+                tokens.addAll(Transaction.fromTrustChainTransactionObject(currentBlock.transaction).tokens)
+            }
+            currentBlock = trustChainCommunity.database.getBlockAfter(currentBlock, TOKEN_BLOCK_TYPE)
+        }
+        return tokens
+    }
 
     companion object {
         const val TOKEN_BLOCK_TYPE = "token_block"
