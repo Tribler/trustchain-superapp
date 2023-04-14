c59cd206b3c2783e7e237e91393ef0ebc5501ce2
julio1998 <julrvsio@hotmail.com>
2023-04-14 13:36:15 +0200

    Remove unwanted files

--- musicdao-datafeeder/bin/main/nl/tudelft/trustchain/musicdaodatafeeder/CryptoCurrencyConfig.kt
+++
@@ -1,8 +0,0 @@
-package nl.tudelft.trustchain.musicdaodatafeeder
-
-import org.bitcoinj.params.RegTestParams
-
-object CryptoCurrencyConfig {
-    val networkParams: RegTestParams = RegTestParams.get()
-    const val chainFileName: String = "forwarding-service-regtest"
-}
