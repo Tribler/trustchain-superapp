c486d46936d8aabd17de0a9a95e07db281efa2e0
Chelsea Guan <chelsea.guan99@gmail.com>
2023-04-14 12:08:27 +0200

    Added info hash, magnet link and bytes downloaded to torrent debug screen

--- detoks/src/main/java/nl/tudelft/trustchain/detoks/TorrentFragment.kt
+++ detoks/src/main/java/nl/tudelft/trustchain/detoks/TorrentFragment.kt
@@ -3,6 +3,7 @@ package nl.tudelft.trustchain.detoks
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import com.frostwire.jlibtorrent.TorrentHandle
 import nl.tudelft.ipv8.android.IPv8Android
 import nl.tudelft.trustchain.common.ui.BaseFragment
 
@@ -10,8 +11,6 @@ import nl.tudelft.trustchain.common.ui.BaseFragment
 class TorrentFragment : BaseFragment(R.layout.fragment_torrent) {
 
     private lateinit var torrentManager: TorrentManager
-    val deToksCommunity = IPv8Android.getInstance().getOverlay<DeToksCommunity>()!!
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         torrentManager = TorrentManager.getInstance(requireActivity())
@@ -20,19 +19,28 @@ class TorrentFragment : BaseFragment(R.layout.fragment_torrent) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        var peerText = view.findViewById<TextView>(R.id.peerText)
-        var isSeedingText = view.findViewById<TextView>(R.id.isSeedingText)
+        var infoHashTV = view.findViewById<TextView>(R.id.infoHashTextView)
+        var magnetLinkTV = view.findViewById<TextView>(R.id.magnetLinkTextView)
+        var downloadedBytesTV = view.findViewById<TextView>(R.id.downloadedBytesTextView)
 
-        val peerId = "Peer ID: ${deToksCommunity.myPeer.mid}"
-        peerText.text = peerId
+        var passedName = "electricsheep-flock-248-7500-9"
 
-        var torrent = torrentManager.getListOfTorrents()[0]
-        var isSeeding = torrent.status().isSeeding()
-        var seedingStatus = if (isSeeding) {
-            "Seeding status: Seeding"
-        } else {
-            "Seeding status: Not seeding"
+        lateinit var torrent: TorrentHandle
+        for (torrentHandle in torrentManager.getListOfTorrents()) {
+            if (torrentHandle.name().equals(passedName)) {
+                torrent = torrentHandle
+            }
         }
-        isSeedingText.text = seedingStatus
+
+        var infoHashText = "Info hash: " + torrent.infoHash().toString()
+        infoHashTV.text = infoHashText
+
+        var magnetLinkText = "Magnet link: " + torrent.makeMagnetUri()
+        magnetLinkTV.text = magnetLinkText
+
+        var downloadedBytesText = torrent.status().totalDone().toString() + " Bytes downloaded"
+        downloadedBytesTV.text = downloadedBytesText
+
+
     }
 }
