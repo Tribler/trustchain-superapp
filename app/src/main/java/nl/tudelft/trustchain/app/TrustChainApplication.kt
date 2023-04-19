90089417f6fb0ea8900e5fb0a4e050259890ca10
Leon Kempen <l.m.kempen@outlook.com>
2023-04-19 11:53:13 +0200

    Resolved PR comments left earlier

--- app/src/main/java/nl/tudelft/trustchain/app/TrustChainApplication.kt
+++ app/src/main/java/nl/tudelft/trustchain/app/TrustChainApplication.kt
@@ -64,7 +64,6 @@ import nl.tudelft.trustchain.common.eurotoken.TransactionRepository
 import nl.tudelft.trustchain.currencyii.CoinCommunity
 
 import nl.tudelft.trustchain.detoks.community.UpvoteCommunity
-// import nl.tudelft.trustchain.detoks.DeToksCommunity
 import nl.tudelft.trustchain.eurotoken.community.EuroTokenCommunity
 import nl.tudelft.trustchain.eurotoken.db.TrustStore
 import nl.tudelft.trustchain.gossipML.RecommenderCommunity
@@ -122,7 +121,6 @@ class TrustChainApplication : Application() {
                 createIdentityCommunity(),
                 createFOCCommunity(),
                 createUpvoteCommunity()
-                // createDeToksCommunity()
             ),
             walkerInterval = 5.0
         )
@@ -461,13 +459,6 @@ class TrustChainApplication : Application() {
             listOf(randomWalk)
         )
     }
-//    private fun createDeToksCommunity(): OverlayConfiguration<DeToksCommunity> {
-//        val randomWalk = RandomWalk.Factory()
-//        return OverlayConfiguration(
-//            DeToksCommunity.Factory(this),
-//            listOf(randomWalk)
-//        )
-//    }
 
     private fun getIdAlgorithmKey(idFormat: String): BonehPrivateKey {
         val prefs = PreferenceManager.getDefaultSharedPreferences(this)
