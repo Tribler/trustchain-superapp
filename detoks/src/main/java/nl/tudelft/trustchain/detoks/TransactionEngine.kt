3f1eff514b6347ad6b64d6f0b17a67ed7f33ac4c
Kostas Andriopoulos <konstantinosand@gmail.com>
2023-04-19 14:20:40 +0200

    detoks: Simplify the `TransactionEngine` class

--- detoks/src/main/java/nl/tudelft/trustchain/detoks/TransactionEngine.kt
+++ detoks/src/main/java/nl/tudelft/trustchain/detoks/TransactionEngine.kt
@@ -1,79 +1,40 @@
 package nl.tudelft.trustchain.detoks
 
+import android.util.Log
 import nl.tudelft.ipv8.Community
 import nl.tudelft.ipv8.Overlay
 import nl.tudelft.ipv8.Peer
-import nl.tudelft.ipv8.attestation.trustchain.BlockBuilder
 import nl.tudelft.ipv8.attestation.trustchain.TrustChainBlock
-import nl.tudelft.ipv8.attestation.trustchain.payload.HalfBlockBroadcastPayload
 import nl.tudelft.ipv8.attestation.trustchain.payload.HalfBlockPayload
 import nl.tudelft.ipv8.messaging.Packet
-import nl.tudelft.ipv8.util.random
 import java.util.*
-import mu.KotlinLogging
 
 
 open class TransactionEngine (override val serviceId: String): Community() {
-    private val broadcastFanOut = 25
-    private val ttl = 100
-    private val logger = KotlinLogging.logger {}
 
-    object MessageId {
-        const val HALF_BLOCK: Int = 11
-        const val HALF_BLOCK_ENCRYPTED: Int = 12
-        const val HALF_BLOCK_BROADCAST: Int = 13
-        const val HALF_BLOCK_BROADCAST_ENCRYPTED: Int = 14
+    fun sendTransaction(block: TrustChainBlock,
+                        peer: Peer,
+                        encrypt: Boolean = false,
+                        msgID: Int) {
+        Log.d("TransactionEngine",
+              "Creating transaction to peer with public key: " + peer.key.pub().toString())
+        sendBlockToRecipient(peer, block, encrypt, msgID)
     }
 
-    init {
-        messageHandlers[MessageId.HALF_BLOCK] = ::onHalfBlockPacket
-        messageHandlers[MessageId.HALF_BLOCK_BROADCAST] = ::onHalfBlockBroadcastPacket
-        messageHandlers[MessageId.HALF_BLOCK_ENCRYPTED] = ::onHalfBlockPacket
-        messageHandlers[MessageId.HALF_BLOCK_BROADCAST_ENCRYPTED] = ::onHalfBlockBroadcastPacket
-    }
-
-    fun sendTransaction(blockBuilder: BlockBuilder, peer: Peer?, encrypt: Boolean = false) {
-        logger.info { "Sending transaction..." }
-        val block = blockBuilder.sign()
-
-        if (peer != null) {
-            sendBlockToRecipient(peer, block, encrypt)
-        } else {
-            sendBlockBroadcast(block, encrypt)
-        }
-    }
-
-    private fun sendBlockToRecipient(peer: Peer, block: TrustChainBlock, encrypt: Boolean) {
+    private fun sendBlockToRecipient(peer: Peer,
+                                     block: TrustChainBlock,
+                                     encrypt: Boolean,
+                                     msgID: Int) {
         val payload = HalfBlockPayload.fromHalfBlock(block)
 
         val data = if (encrypt) {
-            serializePacket(MessageId.HALF_BLOCK_ENCRYPTED, payload, false, encrypt = true, recipient = peer)
+            serializePacket(
+                msgID, payload, false, encrypt = true, recipient = peer, peer = myPeer
+            )
         } else {
-            serializePacket(MessageId.HALF_BLOCK, payload, false)
+            serializePacket(msgID, payload, false, peer = myPeer)
         }
-
         send(peer, data)
-    }
-
-    private fun sendBlockBroadcast(block: TrustChainBlock, encrypt: Boolean) {
-        val payload = HalfBlockBroadcastPayload.fromHalfBlock(block, ttl.toUInt())
-        val randomPeers = getPeers().random(broadcastFanOut)
-        for (randomPeer in randomPeers) {
-            val data = if (encrypt) {
-                serializePacket(MessageId.HALF_BLOCK_BROADCAST_ENCRYPTED, payload, false, encrypt = true, recipient = randomPeer)
-            } else {
-                serializePacket(MessageId.HALF_BLOCK_BROADCAST, payload, false)
-            }
-            send(randomPeer, data)
-        }
-    }
-
-    private fun onHalfBlockPacket(packet: Packet) {
-        logger.info { ("Half block packet received from: " + packet.source.toString()) }
-    }
-
-    private fun onHalfBlockBroadcastPacket(packet: Packet) {
-        logger.info { ("Half block packet received from broadcast from: " + packet.source.toString()) }
     }
 
     override fun onPacket(packet: Packet) {
@@ -102,7 +63,8 @@ open class TransactionEngine (override val serviceId: String): Community() {
                 e.printStackTrace()
             }
         } else {
-            logger.info { "Received unknown message $msgId from $sourceAddress" }
+            Log.d("TransactionEngine",
+                "Received unknown message $msgId from $sourceAddress" )
         }
     }
 
