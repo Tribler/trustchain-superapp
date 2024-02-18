376841e6e3176e30aab5e9d503fff8d5ce7a43e3
Rowdy Mitchell Chotkan <rowdy_chotkan@msn.com>
2024-02-18 16:10:03 +0100

    Add replace `FindViewById` with viewbinding in `common`

--- common/src/main/java/nl/tudelft/trustchain/common/ui/VotesFragmentHelper.kt
+++ common/src/main/java/nl/tudelft/trustchain/common/ui/VotesFragmentHelper.kt
@@ -6,11 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.ListView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import nl.tudelft.trustchain.common.R
+import nl.tudelft.trustchain.common.databinding.FragmentVotesEntryBinding
+import nl.tudelft.trustchain.common.databinding.FragmentVotesTabBinding
 
 /**
  * The adapter for showing the tab fragments in the voting fragment
@@ -35,6 +34,8 @@ class TabsAdapter(fragment: Fragment, private val voters: Array<ArrayList<String
  * The fragment for showing the votes in a list
  */
 class TabFragment : Fragment() {
+    private var _binding: FragmentVotesTabBinding? = null
+    private val binding get() = _binding!!
 
     private lateinit var votesAdapter: VotesAdapter
 
@@ -43,7 +44,8 @@ class TabFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_votes_tab, container, false)
+        _binding = FragmentVotesTabBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -51,13 +53,18 @@ class TabFragment : Fragment() {
             it.containsKey("voters")
         }?.apply {
 
-            val votesList = view.findViewById<ListView>(R.id.votes)
+            val votesList = binding.votes
             votesAdapter = VotesAdapter(
                 view.context,
                 requireArguments().getStringArrayList("voters")!!
             )
             votesList.adapter = votesAdapter
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
 
@@ -82,10 +89,13 @@ class VotesAdapter(
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view =
-            LayoutInflater.from(context).inflate(R.layout.fragment_votes_entry, parent, false)
-
-        view.findViewById<TextView>(R.id.voter_name).text = voters[position]
+        val binding = if (convertView != null) {
+            FragmentVotesEntryBinding.bind(convertView)
+        } else {
+            FragmentVotesEntryBinding.inflate(LayoutInflater.from(context))
+        }
+        val view = binding.root
+        binding.voterName.text = voters[position]
 
         return view
     }
