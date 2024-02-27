a0588521fee89ebe7b67ce9b11f6d7c13f0e0d16
Rowdy Chotkan <22516119+InvictusRMC@users.noreply.github.com>
2024-02-27 11:15:11 +0100

    Cleanup/depedencies (#183)
    * Rename FOC dir to foc
    
    * Remove unused dependencies + import transitive dependencies
    
    * Fix import types
    
    * Delete FOC directory
    
    * Delete test/FOC directory
    
    * Rename FOC in androidTest

--- peerai/src/main/java/nl/tudelft/trustchain/peerai/PeerAIActivity.kt
+++ peerai/src/main/java/nl/tudelft/trustchain/peerai/PeerAIActivity.kt
@@ -1,20 +1,7 @@
 package nl.tudelft.trustchain.peerai
 
-
-import android.os.Bundle
-import android.widget.*
 import nl.tudelft.trustchain.common.BaseActivity
-import smile.nlp.bag
-import smile.nlp.normalize
-import smile.nlp.words
 
-
-open  class PeerAIActivity : BaseActivity() {
-
-    override val navigationGraph = R.navigation.nav_peerai;
-
-
-
-
-
+open class PeerAIActivity : BaseActivity() {
+    override val navigationGraph = R.navigation.nav_peerai
 }
