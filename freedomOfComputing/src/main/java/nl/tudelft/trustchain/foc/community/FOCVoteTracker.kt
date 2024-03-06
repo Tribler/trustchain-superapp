43763ed5f22b6df9ed70342844499979d858232a
François Ezard <francois.ezard@gmail.com>
2024-03-06 17:41:13 +0100

    remove redundant getter

--- freedomOfComputing/src/main/java/nl/tudelft/trustchain/foc/community/FOCVoteTracker.kt
+++ freedomOfComputing/src/main/java/nl/tudelft/trustchain/foc/community/FOCVoteTracker.kt
@@ -5,7 +5,6 @@ import android.util.Log
 class FOCVoteTracker {
     // Stores the votes for all apks
     private val voteMap: MutableMap<String, MutableSet<FOCVote>> = mutableMapOf()
-        get() = field
 
     /**
      * Gets called on pause (or shutdown) of the app to persist state
