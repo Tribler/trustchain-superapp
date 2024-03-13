d29dfca788436a0fef94d23e5bb6f75adb0c61f2
François Ezard <francois.ezard@gmail.com>
2024-03-13 13:48:10 +0100

    fix serializing

--- freedomOfComputing/src/main/java/nl/tudelft/trustchain/foc/community/FOCVoteMessage.kt
+++ freedomOfComputing/src/main/java/nl/tudelft/trustchain/foc/community/FOCVoteMessage.kt
@@ -1,19 +1,32 @@
 package nl.tudelft.trustchain.foc.community
 
 import nl.tudelft.ipv8.messaging.Deserializable
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
 import java.io.Serializable
 
-data class FOCVoteMessage(val fileName: String, val focVote: FOCVote) : Serializable, nl.tudelft.ipv8.messaging.Serializable {
+
+data class FOCVoteMessage(val fileName: String, val focVote: FOCVote) : Serializable,
+    nl.tudelft.ipv8.messaging.Serializable {
     override fun serialize(): ByteArray {
-        return this.serialize()
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        val objectOutputStream = ObjectOutputStream(byteArrayOutputStream)
+        objectOutputStream.writeObject(this)
+        objectOutputStream.flush()
+        return byteArrayOutputStream.toByteArray()
     }
 
+    @Suppress("UNCHECKED_CAST")
     companion object Deserializer : Deserializable<FOCVoteMessage> {
         override fun deserialize(
             buffer: ByteArray,
             offset: Int
         ): Pair<FOCVoteMessage, Int> {
-            return this.deserialize(buffer)
+            val byteArrayInputStream = ByteArrayInputStream(buffer)
+            val objectInputStream = ObjectInputStream(byteArrayInputStream)
+            return objectInputStream.readObject() as Pair<FOCVoteMessage, Int>
         }
     }
 }
