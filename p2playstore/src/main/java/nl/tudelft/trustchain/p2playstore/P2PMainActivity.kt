2cb1c4e0b51b0a8e8077ece943b1a1856d1b3900
matthijsreyers <matthijsreyers@gmail.com>
2025-05-14 13:34:39 +0200

    Fixed nav graph not having unique ID

--- p2playstore/src/main/java/nl/tudelft/trustchain/p2playstore/P2PMainActivity.kt
+++ p2playstore/src/main/java/nl/tudelft/trustchain/p2playstore/P2PMainActivity.kt
@@ -3,7 +3,7 @@ package nl.tudelft.trustchain.p2playstore
 import nl.tudelft.trustchain.common.BaseActivity
 
 class P2PMainActivity() : BaseActivity() {
-    override val navigationGraph = R.navigation.nav_graph
+    override val navigationGraph = R.navigation.nav_graph_p2pstore
 
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
