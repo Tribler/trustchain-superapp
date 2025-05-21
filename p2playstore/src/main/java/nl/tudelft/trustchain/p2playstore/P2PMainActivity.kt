64f73ea0352e54eda5be13ec97a8955ecccdb2c9
matthijsreyers <matthijsreyers@gmail.com>
2025-05-21 12:33:42 +0200

    Copied shared wallet stuff because it cannot be reused

---
+++ p2playstore/src/main/java/nl/tudelft/trustchain/p2playstore/P2PMainActivity.kt
@@ -0,0 +1,8 @@
+package nl.tudelft.trustchain.p2playstore
+
+import android.os.Bundle
+import nl.tudelft.trustchain.common.BaseActivity
+
+class P2PMainActivity() : BaseActivity() {
+    override val navigationGraph = R.navigation.nav_graph_p2pstore
+}
